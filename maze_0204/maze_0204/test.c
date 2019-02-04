720102b60f44c044293a5dc523331ea28b216c4d
Marze000 <1309334291@qq.com>
2019-02-04 22:53:54 +0800

    字符串的排序和叠加

---
+++ maze_0204/maze_0204/test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char paixu(char* str1, char* str2)
+{
+	char* s;
+	char arr[20];
+	s = arr;
+	*s = *str1;
+	if()
+	
+}
+
+
+int main()
+{
+	char* s1 = "mazebuhuirenshu";
+	char* s2 = "woaijisuanji";
+
+	paixu(s1,s2);
+
+	system("pause");
+	return 0;
+}
