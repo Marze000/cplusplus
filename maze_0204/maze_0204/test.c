5b10a69d87769701d263683b8f0b2065011961ab
Marze000 <1309334291@qq.com>
2019-02-05 22:43:43 +0800

    提交新代码

--- maze_0204/maze_0204/test.c
+++ maze_0204/maze_0204/test.c
@@ -2,21 +2,50 @@
 #include <stdlib.h>
 #include <string.h>
 
-char paixu(char* str1, char* str2)
+void paixu(char* str1, char* str2)
 {
 	char* s;
-	char arr[20];
+	char arr[30];
 	s = arr;
-	*s = *str1;
-	if()
-	
+	while (*str1 != '\0' && *str2 != '\0')
+	{
+		if (*str1 < *str2)
+		{
+			*s = *str1;
+			++str1;
+		}
+		else
+		{
+			*s = *str2;
+			++str2;
+		}
+		++s;
+	}
+	if (*str1 == '\0')
+	{
+		while (*str2 != '\0')
+		{
+			*s++ = *str2++;
+		}
+	}
+	if(*str2=='\0')
+	{
+		while (*str1 != '\0')
+		{
+			*s++ = *str1++;
+		}
+	}
+	*s = '\0';
+	s = arr;
+	printf("%s", s);
+
 }
 
 
 int main()
 {
-	char* s1 = "mazebuhuirenshu";
-	char* s2 = "woaijisuanji";
+	char* s1 = "adhlnqrto";
+	char* s2 = "cefksxz";
 
 	paixu(s1,s2);
 
