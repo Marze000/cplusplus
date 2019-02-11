96eb8f7cff99dd76e02423baaa5c4a982b370c00
Marze000 <1309334291@qq.com>
2019-02-11 22:41:19 +0800

    提交新代码

---
+++ maze_0211/maze_0211/test.c
@@ -0,0 +1,43 @@
+#incslude <stdio.h>
+#include <stdlib.h>
+
+union test1
+{
+	int a;
+	double b;
+	float c;
+};
+
+union test2
+{
+	int d;
+	char e;
+};
+
+int main()
+{
+
+	//int r1, r2;
+	//r1 = sizeof(union test1);
+	//r2 = sizeof(union test2);
+	//printf("r1==%d r2==%d", r1, r2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
