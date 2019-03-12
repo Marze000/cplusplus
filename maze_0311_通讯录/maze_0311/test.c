1c333a518b11ed22b3ab358755a3c5a98138eb1b
Marze000 <1309334291@qq.com>
2019-03-12 23:50:06 +0800

    无文件操作的通讯录实现

---
+++ maze_0311_通讯录/maze_0311/test.c
@@ -0,0 +1,222 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "test.h"
+
+int main()
+{
+	member m;
+	Init(&m);
+
+	typedef void(*T)(member *m);
+	//T menu[] = { Insert,Delete,Display,Find };
+	T menu[] = { Insert,Delete,Display,Find,Modify,Clear,Sort };
+	int choice = 0;
+	Menu();
+	printf("\n请输入接下来要操做的选项!\n");
+	scanf("%d", &choice);
+
+	while (1)
+	{
+		if (choice == 0)
+		{
+			printf("再见！\n");
+			system("pause");
+			break;
+		}
+		menu[choice - 1](&m);
+		printf("\n请输入接下来要操做的选项!\n");
+		scanf("%d", &choice);
+	}
+
+	system("pasuse");
+	return 0;
+}
+
+void Menu()
+{
+	printf("************\n");
+	printf("1.添加联系人\n");
+	printf("2.删除联系人\n");
+	printf("3.显示联系人\n");
+	printf("4.查找联系人\n");
+	printf("5.修改联系人\n");
+	printf("6.清空联系人\n");
+	printf("7.对联系人排序\n");
+	printf("0.退出\n");
+	printf("************\n");
+}
+
+void Init(member *m)//初始化联系人
+{
+	if (m == NULL)
+	{
+		return;
+	}
+	m->mem_index = 1;
+}
+
+void Insert(member* m)//添加联系人
+{
+	if (m == NULL)
+	{
+		return;
+	}
+
+	infor *p = &(m->mem[m->mem_index]);
+	printf("亲,输入新的联系人的姓名\n");
+	scanf("%s", p->name);
+
+	printf("输入联系电话号码\n");
+	scanf("%s", p->num);
+
+	++(m->mem_index);//下标加1
+	printf("添加成功！\n");
+	Menu();
+}
+
+void Delete(member *m)//删除联系人
+{
+	if (m == NULL)
+	{
+		return;
+	}
+	int serial_number = 1;
+
+	printf("请输入要删除联系人的编号？");
+	scanf("%d", &serial_number);
+	if (serial_number > 0 && serial_number < m->mem_index)
+	{
+
+
+
+
+	}
+	else
+	{
+		printf("输入有误！");
+		return;
+	}
+
+
+
+}
+
+void Display(member *m)//显示联系人
+{
+	if(m==NULL)
+	{
+		return;
+	}
+	for (int i = 1; i < m->mem_index; ++i)
+	{
+		printf("%d.姓名：%s\t电话：%s\n",i, m ->mem[i].name, m->mem[i].num);
+	}
+}
+
+void Find(member *m)//查找联系人
+{
+	if (m == NULL)
+	{
+		return;
+	}
+	int index = 0;
+	while (1)
+	{
+		printf("请输入你要查找的联系人的编号：\n");
+		scanf("%d", &index);
+		if (index<0 || index>m->mem_index - 1)
+		{
+			printf("输入有误！\n请重新输入：\n");
+			continue;
+		}
+		break;
+	}
+	printf("%d.姓名：%s\t电话：%s\n", 
+		index, m->mem[index].name, m->mem[index].num);
+	printf("查找成功！\n");
+}
+
+void Modify(member *m)//修改联系人
+{
+	int index;
+	int type;
+	if (m == NULL)
+	{
+		return;
+	}
+	printf("请输入你要修改的联系人的编号：");
+	scanf("%d", &index);
+	if (index < 0 || index >( m->mem_index - 1))
+	{
+		printf("输入有误!");
+		return;
+	}
+	infor *p = &m->mem[index];
+	printf("1.姓名\n");
+	printf("2.电话\n");
+	printf("请输入你要修改的类型");
+	scanf("%d", &type);
+	if (type < 1 || type>2)
+	{
+		printf("输入有误！");
+		return;
+	}
+	if (type == 1)
+	{
+		char new_name[256] = { 0 };
+		printf("亲输入联系人的姓名：");
+		scanf("%s", new_name);
+		strcpy(p->name, new_name);
+	}
+	else
+	{
+		char new_num[256] = { 0 };
+		printf("请输入要修改的联系人的电话：");
+		scanf("%s", new_num);
+		strcpy(p->name, new_num);
+	}
+	printf("修改成功！");
+}
+
+void Clear(member *m)//清空通讯录
+{
+	if (m == NULL)
+	{
+		return;
+	}
+	m->mem_index = 0;
+	printf("通讯录已经清空！");
+}
+
+void Swap(infor* m1,infor *m2)
+{
+	char name_tmp[256] = { 0 };
+	char num_tmp[256] = { 0 };
+	strcpy(name_tmp, m1->name);
+	strcpy(m1->name, m2->name);
+	strcpy(m2->name, name_tmp);
+	strcpy(name_tmp, m1->num);
+	strcpy(m1->num, m2->name);
+	strcpy(m2->name, num_tmp);
+}
+
+void Sort(member *m)
+{
+	int i;
+	int j;
+	if (m == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < m->mem_index - 1; ++i)
+	{
+		for (j = 0; j < m->mem_index - 1 - i; ++j)
+		{
+			if (strcmp(m->mem[j].name, m->mem[j + 1].name) > 0)
+			{
+				Swap(&m->mem[j], &m->mem[j + 1]);
+			}
+		}
+	}
+	printf("排序成功！");
+}
+
---
+++ maze_0311_通讯录/maze_0311/test.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include <assert.h>
+
+typedef struct infor//通讯录个人信息												
+{
+	char name[256];
+	char num[256];
+}infor;
+
+typedef struct
+{
+	infor mem[50];//通讯录人数
+	int mem_index;	//通讯录索引--下标
+}member;
+
+void Menu();//菜单
+void Init(member *m);//初始化通讯录
+void Insert(member *m);//添加联系人
+void Delete(member *m);//删除联系人
+void Display(member *m);//显示联系人
+void Find(member *m);//查找联系人
+void Modify(member *m);//修改联系人
+void Clear(member *m);//清空联系人
+
+void Swap(infor *m1, infor *m2);//按姓名排序
+void Sort(member *m);
+
+
+
