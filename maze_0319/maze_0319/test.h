d39fdea9b16db24f0bfba942658c4e7ff9d84cf3
Marze000 <1309334291@qq.com>
2019-03-20 19:07:26 +0800

    链表的增删查改

--- maze_0319/maze_0319/test.h
+++ maze_0319/maze_0319/test.h
@@ -61,6 +61,7 @@ void SListPushBack(SList *s, SListDataType v)
 	cur->next = node;
 }
 
+//尾插
 void SListPushBack2(SList *s, SListDataType v)
 {
 	Node* node = (Node*)malloc(sizeof(Node));
@@ -85,6 +86,7 @@ void SListPushBack2(SList *s, SListDataType v)
 	}
 }
 
+//头删
 void SListPopFront(SList *s)
 {
 	assert(s != NULL);		 //不能没有链表
@@ -94,6 +96,7 @@ void SListPopFront(SList *s)
 	s->first = next;
 }
 
+//尾删
 void SListPopBack(SList *s)
 {
 	assert(s != NULL);
@@ -106,8 +109,8 @@ void SListPopBack(SList *s)
 		return;
 	}
 
-	Node *cur;
-	for (cur = s->first; cur->next->next != NULL;cur = cur->next)
+	Node *cur = s->first;
+	for (; cur->next->next != NULL;cur = cur->next)
 	{
 		;
 	}
@@ -117,13 +120,17 @@ void SListPopBack(SList *s)
 	cur->next = NULL;
 }
 
+//尾删
 void SListPopBack2(SList *s)
 {
+	//只有一个结点
 	if (s->first->next == NULL)
 	{
 		free(s->first);
 		s->first = NULL;
 	}
+
+	//至少一个结点
 	else
 	{
 		Node* cur = s->first;
@@ -136,6 +143,7 @@ void SListPopBack2(SList *s)
 	}
 }
 
+//反转链表
 struct ListNode* reverseList(struct ListNode * head)
 {
 	struct ListNode *newHead = NULL;
@@ -144,6 +152,7 @@ struct ListNode* reverseList(struct ListNode * head)
 	{
 		struct ListNode* node = cur;
 		cur = cur->next;
+
 		//把node 插入到新链表 newHead
 		node->next = newHead;
 		newHead = node;
@@ -151,6 +160,7 @@ struct ListNode* reverseList(struct ListNode * head)
 	return newHead;
 }
 
+//尾插
 void PushBack2(SList *s, SListDataType v)
 {
 	Node *node = (Node *)malloc(sizeof(Node));
@@ -173,6 +183,7 @@ void PushBack2(SList *s, SListDataType v)
 	}
 }
 
+//尾删
 void PopBack2(SList *s)
 {
 	//没有结点，直接assert
@@ -212,6 +223,7 @@ void PopBack2(SList *s)
 
 */
 
+//查找
 Node *SListFind(SList *s, SListDataType v)
 {
 	for (Node *cur = s->first; cur != NULL; cur = cur->next)
@@ -224,7 +236,7 @@ Node *SListFind(SList *s, SListDataType v)
 	return NULL;
 }
 
-//pos 一定是链表中的结点
+//在pos的后面插入一个结点，pos 一定是链表中的结点
 void SListInsertAfter(SList *s , Node* pos,SListDataType v)
 {
 	Node *node = (Node*)malloc(sizeof(Node));
@@ -236,11 +248,12 @@ void SListInsertAfter(SList *s , Node* pos,SListDataType v)
 //pos 一定是链表中的结点，并且pos 不是链表的最后一个结点
 void SListEraseAfter(SList *s, Node *pos)
 {
-	Node *next = pos->next;
+	Node *next = pos->next;//先做一个标记
 	pos->next = pos->next->next;
 	free(next);
 }
 
+//销毁整个链表
 void SListDestroy(SList *s)
 {
 	Node *next;
@@ -252,7 +265,7 @@ void SListDestroy(SList *s)
 	s->first = NULL;
 }
 
-// 反转单链表
+// 反转单链表，定义三个指针的方法，进行翻转
 Node * Reverse(Node *head)
 {
 	Node *prev = NULL;
@@ -266,174 +279,41 @@ Node * Reverse(Node *head)
 		prev = cur;
 		cur = next;
 	}
-
 	return prev;
 }
 
-
-Node ListNode* partition(ListNode* pHead, int x) 
+//删除v所对应的结点
+void SListRemove(SList *s, SListDataType v)
 {
-	ListNode *small = NULL;	// <
-	ListNode *small_last = NULL;	// 比 x 小的最后一个结点
-	ListNode *big = NULL;	// >=
-	ListNode *big_last = NULL;	// 比 x 大于等于的最后一个结点
-
-	for (ListNode *c = pHead; c != NULL; c = c->next)
+	if (s->first == NULL)
 	{
-		if (c->val < x)
-		{
-			if (small_last == NULL)
-			{
-				small = small_last = c;
-			}
-			else
-			{
-				small_last->next = c;
-				small_last = c;
-			}
-		}
-		else
-		{
-			if (big_last == NULL)
-			{
-				big = big_last = c;
-			}
-			else
-			{
-				big_last->next = c;
-				big_last = c;
-			}
-		}
-	}
-
-	if (small_last != NULL)
-	{
-		small_last->next = big;
-	}
-
-	if (big_last != NULL)
-	{
-		big_last->next = NULL;
-	}
-
-	if (small != NULL)
-	{
-		return small;
-	}
-	else
-	{
-		return big;
-	}
-}
-
-ListNode* partition(ListNode* pHead, int x) {
-	ListNode *small = NULL;	// <
-	ListNode *big = NULL;	// >=
-	ListNode *next;
-	for (ListNode *c = pHead; c != NULL; c = next) {
-		next = c->next;
-		if (c->val < x) {
-			c->next = small;
-			small = c;
-		}
-		else {
-			c->next = big;
-			big = c;
-		}
-	}
-
-	small = Reverse(small);
-	big = Reverse(big);
-
-	// ...
-}
-
-ListNode* deleteDuplication(ListNode* pHead) {
-	if (pHead == NULL) {
-		return NULL;
-	}
-
-	ListNode *fake = (ListNode *)malloc(sizeof(ListNode));
-	fake->next = pHead;
-
-	ListNode *prev = fake;	// 永远是 p1 的前驱
-	ListNode *p1 = pHead;	// p1 和 p2 负责进行 val 的比较
-	ListNode *p2 = pHead->next;	// p2 还有个职责，找第一个不相等的结点
-
-	while (p2 != NULL) {
-		if (p1->val != p2->val) {
-			prev = p1;
-			p1 = p2;
-			p2 = p2->next;
-		}
-		else {
-			while (p2 != NULL && p2->val == p1->val) {
-				p2 = p2->next;
-			}
-
-			// 删除的就是 [p1, p2)
-			// 笔试题里先不考虑内存泄漏
-			prev->next = p2;
-
-			p1 = p2;
-			if (p2 != NULL) {
-				p2 = p2->next;
-			}
-		}
-	}
-
-	pHead = fake->next;
-	free(fake);
-
-	return pHead;
-}
-
-void SListRemove(SList *s, SListDataType v) 
-{
-	if (s->first == NULL) 
-	{ 		// 链表为空
+		//链表为空
 		return;
-	} 	
+	}
+
 	if (s->first->value == v)
-	{ 		
-		// v 就是第一个结点的情况
+	{
+		//v就是第一个结点的情况
 		Node *next = s->first->next;
 		free(s->first);
 		s->first = next;
 		return;
-	} 	
+	}
 	Node *cur = s->first;
 	while (cur->next != NULL)
-	{ 		
+	{
 		if (cur->next->value == v)
-		{ 			
+		{
 			Node *next = cur->next->next;
 			free(cur->next);
 			cur->next = next;
 			return;
-		} 		
-		cur = cur->next;
-}	{
-#if 0
-void SListRemove2(SList *s, SListDataType v)
-{ 	
-	if (s->first == NULL)
-	{ 		// 链表为空
-		return;
-	} 	
-	Node *fake = (Node *)malloc(sizeof(Node));
-	fake->next = s->first; 	Node *cur = fake;
-	while (cur->next != NULL)
-	{ 		
-		if ()
-		{
-			...
 		}
 		cur = cur->next;
 	}
-	free(fake);
 }
-#endif
+
+//删除v对应的所有结点
 struct ListNode* removeElements(struct ListNode* head, int val)
 {
 	if (head == NULL)
@@ -441,22 +321,24 @@ struct ListNode* removeElements(struct ListNode* head, int val)
 		return NULL;
 	}
 
-	// 先不管第一个结点的情况
-	struct ListNode *cur = head;
-	while (cur->next != NULL) {
-		if (cur->next->value == val) {
-			struct ListNode *next = cur->next->next;
+	//先不管第一个结点的情况
+	struct ListNode* cur = head;
+	while (cur->next != NULL)
+	{
+		if (cur->next->value == val)
+		{
+			struct ListNode* next = cur->next->next;
 			free(cur->next);
 			cur->next = next;
 		}
-		else {
+		else
+		{
 			cur = cur->next;
 		}
 	}
-
 	if (head->value == val)
 	{
-		Node *newHead = head->next;
+		Node* newHead = head->next;
 		free(head);
 		return newHead;
 	}
@@ -465,81 +347,5 @@ struct ListNode* removeElements(struct ListNode* head, int val)
 		return head;
 	}
 }
-
-class Solution {
-public:
-	ListNode* FindKthToTail(ListNode* pListHead, unsigned int k) {
-		ListNode *front = pListHead;
-		ListNode *back = pListHead;
-		unsigned int i;
-		for (i = 0; front != NULL && i < k; i++) {
-			front = front->next;
-		}
-
-		if (i < k) {
-			return NULL;
-		}
-
-		while (front) {
-			front = front->next;
-			back = back->next;
-		}
-
-		return back;
-	}
-};
-
-struct ListNode *Merge(struct ListNode *l1, struct ListNode *l2) {
-	if (l1 == NULL) {
-		return l2;
-	}
-
-	if (l2 == NULL) {
-		return l1;
-	}
-
-	struct ListNode *c1 = l1;
-	struct ListNode *c2 = l2;
-	struct ListNode *result = NULL;	// 结果链表的第一个结点地址
-	struct ListNode *tail = NULL;	// 结果链表的最后一个结点地址
-	while (c1 != NULL && c2 != NULL) {
-		if (c1->val <= c2->val) {
-			// 取 l1 链表的结点
-			if (tail == NULL) {
-				result = tail = c1;
-			}
-			else {
-				tail->next = c1;
-				tail = c1;
-			}
-
-			c1 = c1->next;
-		}
-		else {
-			// 取 l2 链表的结点
-			if (tail == NULL) {
-				result = tail = c2;
-			}
-			else {
-				tail->next = c2;
-				tail = c2;
-			}
-
-			c2 = c2->next;
-		}
-	}
-
-	// l1 或者 l2 有一条链表的所有结点被去完了
-	if (c1 != NULL) {
-		tail->next = c1;	// c1 代表的不是一个结点，代表的是链表从 c1 的后序所有结点
-	}
-	else {
-		tail->next = c2;
-	}
-
-	return result;
-}
-
-
 
 
