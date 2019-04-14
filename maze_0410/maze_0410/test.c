3b063b6be55dfa4828e8662d9f4f6f8943ac5723
Marze000 <1309334291@qq.com>
2019-04-14 23:49:23 +0800

    练习git push

--- maze_0410/maze_0410/test.c
+++ maze_0410/maze_0410/test.c
@@ -62,6 +62,871 @@ void DestroyHeap(Heap* hp)
 	hp->_capacity = 0;
 	hp->_size = 0;
 }
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+
+}
+
+//É¾³ý¶Ñ¶¥ÔªËØ
+void EraseHeap(Heap* hp)
+{
+	assert(hp);
+	Swap(&(hp->_array[0]), &(hp->_array[size - 1]));
+	AdjustDown(hp->_array, hp->_size - 1, 0);
+}
+
+int SizeHeap(Heap* hp)
+{
+	assert(hp);
+	return hp->_size;
+}
+
+int EmptyHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		assert(hp);
+		return;
+	}
+}
+
+DataType TopHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		return NULL;
+	}
+	return hp;
+}
+//typedef struct Heap
+//{
+//	DataType* _array;
+//	int _capacity;
+//	int _size;
+//}Heap;
+
+void DestroyHeap(Heap* hp)
+{
+	assert(hp);
+	hp->_capacity = 0;
+	hp->_size = 0;
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+
+}
+
+//É¾³ý¶Ñ¶¥ÔªËØ
+void EraseHeap(Heap* hp)
+{
+	assert(hp);
+	Swap(&(hp->_array[0]), &(hp->_array[size - 1]));
+	AdjustDown(hp->_array, hp->_size - 1, 0);
+}
+
+int SizeHeap(Heap* hp)
+{
+	assert(hp);
+	return hp->_size;
+}
+
+int EmptyHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		assert(hp);
+		return;
+	}
+}
+
+DataType TopHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		return NULL;
+	}
+	return hp;
+}
+//typedef struct Heap
+//{
+//	DataType* _array;
+//	int _capacity;
+//	int _size;
+//}Heap;
+
+void DestroyHeap(Heap* hp)
+{
+	assert(hp);
+	hp->_capacity = 0;
+	hp->_size = 0;
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+
+}
+
+//É¾³ý¶Ñ¶¥ÔªËØ
+void EraseHeap(Heap* hp)
+{
+	assert(hp);
+	Swap(&(hp->_array[0]), &(hp->_array[size - 1]));
+	AdjustDown(hp->_array, hp->_size - 1, 0);
+}
+
+int SizeHeap(Heap* hp)
+{
+	assert(hp);
+	return hp->_size;
+}
+
+int EmptyHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		assert(hp);
+		return;
+	}
+}
+
+DataType TopHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		return NULL;
+	}
+	return hp;
+}
+//typedef struct Heap
+//{
+//	DataType* _array;
+//	int _capacity;
+//	int _size;
+//}Heap;
+
+void DestroyHeap(Heap* hp)
+{
+	assert(hp);
+	hp->_capacity = 0;
+	hp->_size = 0;
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+
+}
+
+//É¾³ý¶Ñ¶¥ÔªËØ
+void EraseHeap(Heap* hp)
+{
+	assert(hp);
+	Swap(&(hp->_array[0]), &(hp->_array[size - 1]));
+	AdjustDown(hp->_array, hp->_size - 1, 0);
+}
+
+int SizeHeap(Heap* hp)
+{
+	assert(hp);
+	return hp->_size;
+}
+
+int EmptyHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		assert(hp);
+		return;
+	}
+}
+
+DataType TopHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		return NULL;
+	}
+	return hp;
+}
+//typedef struct Heap
+//{
+//	DataType* _array;
+//	int _capacity;
+//	int _size;
+//}Heap;
+
+void DestroyHeap(Heap* hp)
+{
+	assert(hp);
+	hp->_capacity = 0;
+	hp->_size = 0;
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+
+}
+
+//É¾³ý¶Ñ¶¥ÔªËØ
+void EraseHeap(Heap* hp)
+{
+	assert(hp);
+	Swap(&(hp->_array[0]), &(hp->_array[size - 1]));
+	AdjustDown(hp->_array, hp->_size - 1, 0);
+}
+
+int SizeHeap(Heap* hp)
+{
+	assert(hp);
+	return hp->_size;
+}
+
+int EmptyHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		assert(hp);
+		return;
+	}
+}
+
+DataType TopHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		return NULL;
+	}
+	return hp;
+}
+//typedef struct Heap
+//{
+//	DataType* _array;
+//	int _capacity;
+//	int _size;
+//}Heap;
+
+void DestroyHeap(Heap* hp)
+{
+	assert(hp);
+	hp->_capacity = 0;
+	hp->_size = 0;
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+
+}
+
+//É¾³ý¶Ñ¶¥ÔªËØ
+void EraseHeap(Heap* hp)
+{
+	assert(hp);
+	Swap(&(hp->_array[0]), &(hp->_array[size - 1]));
+	AdjustDown(hp->_array, hp->_size - 1, 0);
+}
+
+int SizeHeap(Heap* hp)
+{
+	assert(hp);
+	return hp->_size;
+}
+
+int EmptyHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		assert(hp);
+		return;
+	}
+}
+
+DataType TopHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		return NULL;
+	}
+	return hp;
+}
+//typedef struct Heap
+//{
+//	DataType* _array;
+//	int _capacity;
+//	int _size;
+//}Heap;
+
+void DestroyHeap(Heap* hp)
+{
+	assert(hp);
+	hp->_capacity = 0;
+	hp->_size = 0;
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+
+}
+
+//É¾³ý¶Ñ¶¥ÔªËØ
+void EraseHeap(Heap* hp)
+{
+	assert(hp);
+	Swap(&(hp->_array[0]), &(hp->_array[size - 1]));
+	AdjustDown(hp->_array, hp->_size - 1, 0);
+}
+
+int SizeHeap(Heap* hp)
+{
+	assert(hp);
+	return hp->_size;
+}
+
+int EmptyHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		assert(hp);
+		return;
+	}
+}
+
+DataType TopHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		return NULL;
+	}
+	return hp;
+}
+//typedef struct Heap
+//{
+//	DataType* _array;
+//	int _capacity;
+//	int _size;
+//}Heap;
+
+void DestroyHeap(Heap* hp)
+{
+	assert(hp);
+	hp->_capacity = 0;
+	hp->_size = 0;
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+
+}
+
+//É¾³ý¶Ñ¶¥ÔªËØ
+void EraseHeap(Heap* hp)
+{
+	assert(hp);
+	Swap(&(hp->_array[0]), &(hp->_array[size - 1]));
+	AdjustDown(hp->_array, hp->_size - 1, 0);
+}
+
+int SizeHeap(Heap* hp)
+{
+	assert(hp);
+	return hp->_size;
+}
+
+int EmptyHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		assert(hp);
+		return;
+	}
+}
+
+DataType TopHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		return NULL;
+	}
+	return hp;
+}
+//typedef struct Heap
+//{
+//	DataType* _array;
+//	int _capacity;
+//	int _size;
+//}Heap;
+
+void DestroyHeap(Heap* hp)
+{
+	assert(hp);
+	hp->_capacity = 0;
+	hp->_size = 0;
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+
+}
+
+//É¾³ý¶Ñ¶¥ÔªËØ
+void EraseHeap(Heap* hp)
+{
+	assert(hp);
+	Swap(&(hp->_array[0]), &(hp->_array[size - 1]));
+	AdjustDown(hp->_array, hp->_size - 1, 0);
+}
+
+int SizeHeap(Heap* hp)
+{
+	assert(hp);
+	return hp->_size;
+}
+
+int EmptyHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		assert(hp);
+		return;
+	}
+}
+
+DataType TopHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		return NULL;
+	}
+	return hp;
+}
+//typedef struct Heap
+//{
+//	DataType* _array;
+//	int _capacity;
+//	int _size;
+//}Heap;
+
+void DestroyHeap(Heap* hp)
+{
+	assert(hp);
+	hp->_capacity = 0;
+	hp->_size = 0;
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+
+}
+
+//É¾³ý¶Ñ¶¥ÔªËØ
+void EraseHeap(Heap* hp)
+{
+	assert(hp);
+	Swap(&(hp->_array[0]), &(hp->_array[size - 1]));
+	AdjustDown(hp->_array, hp->_size - 1, 0);
+}
+
+int SizeHeap(Heap* hp)
+{
+	assert(hp);
+	return hp->_size;
+}
+
+int EmptyHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		assert(hp);
+		return;
+	}
+}
+
+DataType TopHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		return NULL;
+	}
+	return hp;
+}
+//typedef struct Heap
+//{
+//	DataType* _array;
+//	int _capacity;
+//	int _size;
+//}Heap;
+
+void DestroyHeap(Heap* hp)
+{
+	assert(hp);
+	hp->_capacity = 0;
+	hp->_size = 0;
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+
+}
+
+//É¾³ý¶Ñ¶¥ÔªËØ
+void EraseHeap(Heap* hp)
+{
+	assert(hp);
+	Swap(&(hp->_array[0]), &(hp->_array[size - 1]));
+	AdjustDown(hp->_array, hp->_size - 1, 0);
+}
+
+int SizeHeap(Heap* hp)
+{
+	assert(hp);
+	return hp->_size;
+}
+
+int EmptyHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		assert(hp);
+		return;
+	}
+}
+
+DataType TopHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		return NULL;
+	}
+	return hp;
+}
+//typedef struct Heap
+//{
+//	DataType* _array;
+//	int _capacity;
+//	int _size;
+//}Heap;
+
+void DestroyHeap(Heap* hp)
+{
+	assert(hp);
+	hp->_capacity = 0;
+	hp->_size = 0;
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+
+}
+
+//É¾³ý¶Ñ¶¥ÔªËØ
+void EraseHeap(Heap* hp)
+{
+	assert(hp);
+	Swap(&(hp->_array[0]), &(hp->_array[size - 1]));
+	AdjustDown(hp->_array, hp->_size - 1, 0);
+}
+
+int SizeHeap(Heap* hp)
+{
+	assert(hp);
+	return hp->_size;
+}
+
+int EmptyHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		assert(hp);
+		return;
+	}
+}
+
+DataType TopHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		return NULL;
+	}
+	return hp;
+}
+//typedef struct Heap
+//{
+//	DataType* _array;
+//	int _capacity;
+//	int _size;
+//}Heap;
+
+void DestroyHeap(Heap* hp)
+{
+	assert(hp);
+	hp->_capacity = 0;
+	hp->_size = 0;
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+
+}
+
+//É¾³ý¶Ñ¶¥ÔªËØ
+void EraseHeap(Heap* hp)
+{
+	assert(hp);
+	Swap(&(hp->_array[0]), &(hp->_array[size - 1]));
+	AdjustDown(hp->_array, hp->_size - 1, 0);
+}
+
+int SizeHeap(Heap* hp)
+{
+	assert(hp);
+	return hp->_size;
+}
+
+int EmptyHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		assert(hp);
+		return;
+	}
+}
+
+DataType TopHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		return NULL;
+	}
+	return hp;
+}
+//typedef struct Heap
+//{
+//	DataType* _array;
+//	int _capacity;
+//	int _size;
+//}Heap;
+
+void DestroyHeap(Heap* hp)
+{
+	assert(hp);
+	hp->_capacity = 0;
+	hp->_size = 0;
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+	//hp->next - null
+}
+
+//É¾³ý¶Ñ¶¥ÔªËØ
+void EraseHeap(Heap* hp)
+{
+	assert(hp);
+	Swap(&(hp->_array[0]), &(hp->_array[size - 1]));
+	AdjustDown(hp->_array, hp->_size - 1, 0);
+}
+
+int SizeHeap(Heap* hp)
+{
+	assert(hp);
+	return hp->_size;
+}
+
+int EmptyHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		assert(hp);
+		return;
+	}
+}
+
+DataType TopHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		return NULL;
+	}
+	return hp;
+}
+//typedef struct Heap
+//{
+//	DataType* _array;
+//	int _capacity;
+//	int _size;
+//}Heap;
+
+void DestroyHeap(Heap* hp)
+{
+	assert(hp);
+	hp->_capacity = 0;
+	hp->_size = 0;
+}
+//²åÈë
+void InsertHeap(Heap* hp, DataType data)
+{
+
+}
+
+//É¾³ý¶Ñ¶¥ÔªËØ
+void EraseHeap(Heap* hp)
+{
+	assert(hp);
+	Swap(&(hp->_array[0]), &(hp->_array[size - 1]));
+	AdjustDown(hp->_array, hp->_size - 1, 0);
+}
+
+int SizeHeap(Heap* hp)
+{
+	assert(hp);
+	return hp->_size;
+}
+
+int EmptyHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		assert(hp);
+		return;
+	}
+}
+
+DataType TopHeap(Heap* hp)
+{
+	if (hp == NULL)
+	{
+		return NULL;
+	}
+	return hp;
+}
+//typedef struct Heap
+//{
+//	DataType* _array;
+//	int _capacity;
+//	int _size;
+//}Heap;
+
+void DestroyHeap(Heap* hp)
+{
+	assert(hp);
+	hp->_capacity = 0;
+	hp->_size = 0;
+}
 
 
 
@@ -69,7 +934,6 @@ void DestroyHeap(Heap* hp)
 int main()
 {
 	int a[] = { 27,15,19,18,28,34,65,49,25,37 };
-
 
 
 
