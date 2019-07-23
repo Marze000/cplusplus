b41cce257cc0350ed7892b7bc5fc2dcc397c9d77
Marze000 <1309334291@qq.com>
2019-07-23 16:41:55 +0800

    排序算法

--- maze_0721OJ/maze_0721OJ/test.cpp
+++ maze_0721OJ/maze_0721OJ/test.cpp
@@ -1,8 +1,9 @@
-#if 0
+
+
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
-
+#if 1
 int main() {
 	long long CD[3] = {0, 0,1 };
 	long long JC[3] = {0, 1,2 };
@@ -114,10 +115,13 @@ int main(){
 using namespace std;
 
 //通过一套快速排序将数列分为两部分
-int Partion(int* arr, int left, int right) {
+#if 1
+int Partion1(int* arr, int left, int right) {
 	int key = arr[right - 1];
 	int begin = left;
 	int end = right - 1;
+	//设置两个下标 begin 指向首元素，end 指向尾元素
+	// 把最后一个元素保存在一个变量中，如果第一个首下标小与key就一直往前走
 	while (begin < end) {
 		while (begin < end && arr[begin] <= key) {
 			++begin;
@@ -125,26 +129,127 @@ int Partion(int* arr, int left, int right) {
 		while (begin < end && arr[end] >= key) {
 			--end;
 		}
-
+		//如果两个下标对应的是同一个值也就不需要交换了
 		if (begin < end) {
 			swap(arr[end], arr[begin]);
 		}
 	}
-
+	//如果begin 在最后一个位置的话说明前面的值都比key小
+	// 也就不需要交换了
 	if (begin != right - 1) {
-		swap(arr[end], arr[right -1]);
+		swap(arr[begin], arr[right -1]);
 	}
-
 	return begin;
 }
 
 void QuickSort(int* array, int left, int right) {
 	if (right - left > 1) {
 		//划分基准值
-		int div = Partion(array, left, right);
+		int div = Partion1(array, left, right);
 
 		QuickSort(array, left, div);
 		QuickSort(array, div + 1, right);
+	}
+}
+
+int Partion1(int* array, int left, int right) {
+	int begin = left;
+	int end = right - 1;
+	int key = array[right - 1];
+
+	while (begin < end) {
+		while (begin < end && array[begin] <= key){
+			++begin;
+		}
+		while (begin < end && array[end] >= key) {
+			--end;
+		}
+
+		if (begin < end) {
+			swap(array[begin], array[end]);
+		}
+	}
+	if (begin != right - 1) {
+		swap(array[begin], array[right - 1]);
+	}
+	return begin;
+}
+
+int Partion2(int* array, int left, int right) {
+	int begin = left;
+	int end = right - 1;
+	int key = array[right - 1];
+
+	while (begin < end) {
+		while (begin < end && array[begin] <= key) {
+			++begin;
+		}
+		if (begin < end) {
+			array[end] = array[begin];
+			--end;
+		}
+
+		while (begin < end && array[end] >= key) {
+			--end;
+		}
+		if (begin < end) {
+			array[begin] = array[end];
+			++begin;
+		}
+	}
+	array[begin] = key;
+	return begin;
+}
+
+int Partion3(int* array, int left, int right) {
+	int key = array[right - 1];
+	int cur = left;
+	int pre = cur - 1;
+
+	while (cur < right) {
+		if (array[cur] < key && ++pre != cur) {
+			swap(array[cur], array[pre]);
+		}
+		++cur;
+	}
+	if (++pre != right - 1) {
+		swap(array[pre], array[right - 1]);
+	}
+	return pre;
+}
+
+
+void QuickSort(int* array, int left, int right) {
+	if (right - left  > 1) {
+		//int div = Partion1(array, left, right);
+		//int div = Partion2(array, left, right);
+		int div = Partion3(array, left, right);
+
+		QuickSort(array, left, div);
+		QuickSort(array, div + 1, right);
+	}
+}
+
+#include <stack>
+void QuickSortNor(int* array, int size) {
+	int left = 0;
+	int right = size;
+
+	stack<int>st;
+	st.push(right);
+	st.push(left);
+	while (!st.empty()) {
+		left = st.top();
+		st.pop();
+		right = st.top();
+		st.pop();
+		if (left < right) {
+			int div = Partion2(array, left, right);
+			st.push(right);
+			st.push(div + 1);
+			st.push(div);
+			st.push(left);
+		}
 	}
 }
 
@@ -153,6 +258,139 @@ int main() {
 	QuickSort(a, 0, 10);
 	for (int i = 0; i < 10; ++i) {
 		cout << a[i] << " ";
+	}
+	cout << endl;
+
+	int b[10] = { 3,5,1,6,9,0,7,2,8,4 };
+	QuickSortNor(b, 10);
+	for (int i = 0; i < 10; ++i) {
+		cout << b[i] << " ";
+	}
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
+#endif
+
+#if 1
+void BubbleSort(int* array, int size) {
+	for (int i = 0; i < size - 1; ++i) {
+		int flag = 0;
+		for (int j = 1; j < size - i; ++j) {
+			if (array[j-1] > array[j]) {
+				swap(array[j], array[j - 1]);
+				flag = 1;
+			}
+		}
+		if (flag == 0) {
+			return;
+		}
+	}
+}
+
+void BubbleSort(int* array, int size) {
+	for (int i = 0; i < size - 1; ++i) {
+		int flag = 1;
+		for (int j = size-1; j > 0; --j) {
+			if (array[j] < array[j - 1]) {
+				swap(array[j], array[j - 1]);
+				flag = 0;
+			}
+		}
+		if (flag == 1) {
+			return;
+		}
+	}
+}
+#endif
+
+//选择排序法 大致思路：
+//如果是升序的话 先整体遍历找到最大的元素，让它和最后一个元素交换
+// 每交换一次，遍历的范围就缩小一次，直到将整个序列排好
+#if 1
+void SelectSortLess(int* array, int size) {
+	for (int i = 0; i < size - 1; ++i) {
+		int maxpos = 0;
+		//找到最大元素的下标
+		for (int j = 1; j < size - i; ++j) {
+			if (array[maxpos] < array[j]) {
+				maxpos = j;
+			}
+		}
+		if (maxpos != size - i - 1) {
+			swap(array[maxpos], array[size - i - 1]);
+		}
+	}
+}
+
+void SelectSortGreat(int* array, int size) {
+	for (int i = 0; i < size - 1; ++i) {
+		int minpos = 0;
+		for (int j = 0; j < size - i; ++j) {
+			if (array[minpos] > array[j]) {
+				minpos = j;
+			}
+		}
+		if (minpos != size - i - 1) {
+			swap(array[minpos], array[size - i - 1]);
+		}
+	}
+}
+#endif
+
+void AdjustDown(int* array, int size, int last) {
+	int child = last * 2 + 1;
+	while (child < size) {
+		if (child + 1 < size && array[child] < array[child + 1]) {
+			++child;
+		}
+		if (array[child] > array[last]) {
+			swap(array[child], array[last]);
+			last = child;
+			child = 2 * last + 1;
+		}
+		else {
+			return;
+		}
+	}
+}
+
+void HeapSort(int* array, int size) {
+	int last = (size - 2) / 2;
+	//建堆
+	for (; last >= 0; --last) {
+		AdjustDown(array, size,last);
+	}
+
+	//删除堆顶元素
+	int end = size - 1;
+	while (end) {
+		swap(array[end], array[0]);
+		AdjustDown(array, end,0);
+		end--;
+	}
+}
+
+int main() {
+
+	int b[10] = { 3,5,1,6,9,0,7,2,8,4 };
+	HeapSort(b, 10);
+	for (int i = 0; i < 10; ++i) {
+		cout << b[i] << " ";
+	}
+	cout << endl;
+	
+	int b[10] = { 3,5,1,6,9,0,7,2,8,4 };
+	SelectSortLess(b, 10);
+	for (int i = 0; i < 10; ++i) {
+		cout << b[i] << " ";
+	}
+	cout << endl;
+
+	SelectSortGreat(b, 10);
+	for (int i = 0; i < 10; ++i) {
+		cout << b[i] << " ";
 	}
 	cout << endl;
 
