b8538d287ccba5d6fc371a05e4d8fc3fa2728618
Marze000 <1309334291@qq.com>
2019-08-10 21:50:55 +0800

    C++AVL树

--- maze_0809_AVL/maze_0809_AVL/test.cc
+++ maze_0809_AVL/maze_0809_AVL/test.cc
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <stdlib.h>
+using namespace std;
+
 
 template<class T>
 struct AVLTreeNode{
-	AVLTreeNode(const T& data)
+	AVLTreeNode(const T& data = T())
 		: _pLeft(nullptr), _pRight(nullptr), _pParent(nullptr)
 		, _data(data)
 		, _bf(0)//平衡因子
@@ -17,21 +19,105 @@ struct AVLTreeNode{
 };
 
 template<class T>
-class BSTree{
-	typedef BSTNode<T> Node;
-	typedef Node* PNode;
+class AVLTree{
+	typedef AVLTreeNode<T> Node;
 public:
-	BSTree() : _pRoot(nullptr)
+	AVLTree() : _pRoot(nullptr)
 	{}
-	~BSTree();
-	
+	bool Insert(const T& _pRoot) {
+		if (nullptr == _pRoot) {
+			_pRoot = new Node(data);
+			return true;
+		}
+		//非空
+		Node* pCur = _pRoot;
+		Node* pParent = nullptr;
+		while (pCur) {
+			pParent = pCur;
+			if (data < pCur->_data){
+				pCur = pCur->_pLeft;
+			}
+			else if (data > pCur->_data) {
+				pCur = pCur->_pRight;
+			}
+			else {
+				return false;
+			}
+		}
+		//插入新节点
+		pCur = new Node(data);
+		if (data < pParent->_data) {
+			pParent->_pLeft = pCur;
+		}else {
+			pParent->_pRight = pCur;
+		}
+
+		//检测平衡因子
+		if (pCur == pParent->_pLeft) {
+			pParent->_bf--;
+		}else {
+			pParent->_bf++;
+		}
+
+		//可能导致 parent 节点的平衡因子不满足 AVL树的性质
+		while (pParent) {
+			if (pParent->_bf == 0) {
+				return true;
+			}
+			else if (-1 == pParent->_bf || 1 == pParent->_bf) {
+				pCur = pParent;
+				pParent = pCur->_pParent;
+			}
+			else {
+				//双亲的平衡因子不满足 AVL 树的性质
+				//双亲的节点的平衡因子为 2 或者 -2
+				//需要对 以双亲为根的二叉树进行旋转处理
+				if (pParent->_bf == 2) {
+					//右子树高
+					if (1 == pCur->_bf) {
+						_RotateL(pParent);
+					}
+					else {
+						//双旋
+						_RotateRL(pParent);
+					}
+				}
+				else {
+					//左子树高
+					if (-1 == pCur->_bf) {
+						_RotateR(pParent);
+					}
+					else {
+						//双旋
+						_RotateLR(pParent);
+					}
+				}
+			}
+			break;
+		}
+		return true;
+	}
+
+	void InOrder() {
+		_InOrder(_pRoot);
+	}
+protected:
+
+	void _InOrder(Node* pRoot) {
+		if (pRoot) {
+			_InOrder(pRoot->_pLeft);
+			cout << pRoot->_data << " ";
+			_InOrder(pRoot->_pRight);
+		}
+	}
+
 	//当根节点的平衡因子小于 -1 的时候，说明左边失衡，需要
 	//右单旋
-	void _RotateR(PNode pParent){
+	void _RotateRight1(Node* pParent){
 		// pSubL: pParent的左孩子
 		// pSubLR: pParent左孩子的右孩子，注意：该
-		PNode pSubL = pParent->_pLeft;
-		PNode pSubLR = pSubL->_pRight;
+		Node* pSubL = pParent->_pLeft;
+		Node* pSubLR = pSubL->_pRight;
 		// 旋转完成之后，30的右孩子作为双亲的左孩子
 		pParent->_pLeft = pSubLR;
 		// 如果30的左子树的右孩子存在，更新亲双亲
@@ -41,7 +127,7 @@ public:
 		// 60 作为 30的右孩子
 		pSubL->_pRight = pParent;
 		// 因为60可能是棵子树，因此在更新其双亲前必须先保存60的双亲
-		PNode pPParent = pParent->_pParent;
+		Node* pPParent = pParent->_pParent;
 		// 更新60的双亲
 		pParent->_pParent = pSubL;
 		// 更新30的双亲
@@ -64,7 +150,88 @@ public:
 		pParent->_bf = pSubL->_bf = 0;
 	}
 
+	void _RotateR(Node* pParent) {
+		Node* pSubL = pParent->_pLeft;
+		Node* pSubLR = pParent->_pRight;
+		
+		//更新孩子指针域
+		pParent->_pLeft = pSubL;
+		if (pSubLR) {
+			pSubLR->_pParent = pParent;
+		}
 
+		pSubL->_pRight = pParent;
+		//更新双亲指针域
+		Node* pPParent = pParent->_pParent;
+		pParent->_pParent = pSubL;
+		pSubL->_pParent = pPParent;
+		if (nullptr == pPParent) {
+			_pRoot = pSubL;
+		}
+		else {
+			if (pParent == pPParent->_pLeft) {
+				pPParent->_pLeft = pSubL;
+			}
+			else {
+				pPParent->_pRight = pSubL;
+			}
+		}
+	}
+
+	//左单旋
+	void _RotateL(Node* pParent) {
+		Node* pSubR = pParent->_pRight;
+		Node* pSubRL = pSubR->_pLeft;
+		
+		pParent->_pRight = pSubRL;
+		if (pSubRL) {
+			pSubRL->_pParent = pParent;
+		}
+
+		pSubR->_pLeft = pParent;
+		Node* pPParent = pParent->_pParent;
+		pSubR->_pParent = pPParent;
+		pParent->_pParent = pSubR;
+
+		if (pPParent == nullptr){
+			_pRoot = pParent;
+		}
+		else {
+			if (pParent = pPParent->_pLeft) {
+				pPParent->_pLeft = pSubR;
+			}
+			else {
+				pPParent->_pRight = pSubR;
+			}
+		}
+		pParent->_bf = pSubR->_bf = 0;
+	}
+
+	void _RotateRL(Node* pParent) {
+		_RotateR(pParent->_pRight);
+		_RotateL(pParent);
+	}
+
+	void _RotateLR(Node* pParent) {
+		_RotateL(pParent->_pLeft);
+		_RotateR(pParent);
+	}
 private:
-	PNode _pRoot;
+	Node* _pRoot;
 };
+
+void TestAVLTree() {
+	int array[] = { 16,3,6,45,32,5,76,23,54,67,98 };
+	AVLTree<int> t;
+	for (auto e : array) {
+		t.Insert(e);
+	}
+	t.InOrder();
+}
+
+int main() {
+
+	TestAVLTree();
+	system("pause");
+	return 0;
+}
