844eab482d1d50058ea62f7315222d8ef3ee8d32
Marze000 <1309334291@qq.com>
2019-09-19 21:57:24 +0800

    test

--- maze_0917/maze_0917/head.h
+++ maze_0917/maze_0917/head.h
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
 void dis();
 
--- maze_0917/maze_0917/test.cpp
+++ maze_0917/maze_0917/test.cpp
@@ -1,8 +1,246 @@
 #include "head.h"
 
+bool IsDuplicateNumber(int *array, int n){
+	if (array == NULL) 
+		return false;
+
+	int i, temp;
+	for (i = 0; i < n; i++){
+		while (array[i] != i){
+			if (array[array[i]] == array[i]) {
+				return true;
+			}
+			temp = array[array[i]];
+			array[array[i]] = array[i];
+			array[i] = temp;
+		}
+	}
+	return false;
+}
+
+//序列化：必须保存一个中序遍历结果，然后外加一个前序或者后序遍历结果
+//反序列化：根据两次遍历生成的结果恢复二叉树，代码如下(前序和中序)：
+TreeNode* helper(vector<int>pre, int startPre, int endPre,
+	vector<int>in, int startIn, int endIn){
+	if (startPre > endPre || startIn > endIn) {
+		return nullptr;
+	}
+	TreeNode * root = new TreeNode(pre[startPre]);
+	for (int i = startIn; i <= endIn; ++i){
+		if (in[i] == pre[startPre]){
+			root->left = helper(pre, startPre + 1, startPre + i - startIn,
+								in, startIn, i - 1);
+
+			root->right = helper(pre, i - startIn + startPre + 1, endPre,
+								in, i + 1, endIn);
+
+			break;
+		}
+	}
+	return root;
+}
+TreeNode* reConstructBinaryTree(vector<int> pre, vector<int> vin){
+	TreeNode *root = helper(pre, 0, pre.size() - 1, vin, 0, vin.size() - 1);
+	return root;
+}
+
+#if 0
+void replaceSpace(const char *str, int length) {
+	if (str == nullptr || length <= 0) {
+		return;
+	}
+	int num = 0;
+	for (int i = 0; i < length; ++i) {
+		if (str[i] == ' ') {
+			++num;
+		}
+	}
+	int len = length + (num * 2);
+	vector<char>vec(len);
+	int j = 0;
+	for (int i = 0; i < length; ++i) {
+		if (str[i] == '  ') {
+			vec[j++] = '%';
+			vec[j++] = '2';
+			vec[j++] = '0';
+		}
+		else {
+			vec[j++] = str[i];
+		}
+	}
+	for (int i = 0; i < len; ++i) {
+		cout << vec[i];
+	}
+	cout << endl;
+}
+
+#endif
+#if 0
+void replaceSpace(const char *str, int length) {
+	int num = 0;
+	for (int i = 0; i < length; ++i) {
+		if (str[i] == ' ') {
+			++num;
+		}
+	}
+	int len = length + num * 2 ;
+	vector<char>vec;
+	vec.reserve(len);
+	for (int i = 0; i < length; ++i) {
+		if (str[i] != ' ') {
+			vec.push_back(str[i]);
+		}
+		else {
+			vec.push_back('%');
+			vec.push_back('2');
+			vec.push_back('0');
+		}
+	}
+	for (int i = 0; i < len; ++i) {
+		cout << vec[i];
+	}
+	cout << endl;
+}
+#endif
+int main() {
+	const char* str = "hello world";
+	int length = strlen(str);
+	replaceSpace(str, length);
+
+	system("pause");
+	return 0;
+}
+#if 0
+int main(){
+	int n;
+	while (cin >> n) {
+		int a[n], dp[n][n];
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+			dp[i][i] = a[i];
+		}
+		for (int i = 1; i < n; i++) {
+			for (int j = i - 1; j >= 0; j--) {
+				if (a[i] == a[j]) {
+					dp[j][i] = dp[j + 1][i - 1] + 2 * a[j];
+				}
+				else {
+					dp[j][i] = min(dp[j + 1][i] + 2 * a[j],
+						dp[j][i - 1] + 2 * a[i]);
+				}
+			}
+			cout << dp[0][n - 1] << endl;
+		}
+		return 0;
+	}
+}
+
+int BackMinSum(vector<int>&a, int sum) {
+	int length = a.size();
+	int left = length - 1;
+	int right = length + 1;
+	//43 35 14 51 2 15 42 39 38 38 39 42 15 2 51 14 35 57 43 
+	while (left >= 0 && right < a.size()) {
+		if (a[left] < a[right]) {
+			sum += a[left];
+			--left;
+		}
+		else if (a[left] > a[right]) {
+			sum += a[right];
+			--right;
+		}
+		else {
+			--left;
+			++right;
+		}
+	}
+	for (int i = 0; i < a.size(); ++i) {
+		sum += a[i];
+	}
+	return sum;
+}
 
 
-#if 1
+int BackMinSum(vector<int>&a, int sum) {
+	int start = 0;
+	int end = a.size() - 1;
+	while (start < end) {
+		if (a[start] < a[end]) {
+			sum += a[start];
+			++start;
+		}
+		else if (a[start] > a[end]) {
+			sum += a[end];
+			--end;
+		}
+		else {
+			--end;
+			++start;
+		}
+	}
+	for (int i = 0; i < a.size(); ++i) {
+		sum += a[i];
+	}
+
+	return sum;
+}
+
+int main() {
+	int n = 0;
+	while (cin >> n) {
+		vector<int>a;
+		a.resize(n);
+		for (int i = 0; i < n; ++i) {
+			cin >> a[i];
+		}
+		int sum = 0;
+		cout << BackMinSum(a, sum) << endl;
+	}
+
+	system("pause");
+	return 0;
+}
+
+bool Find(int target, vector<vector<int> > array) {
+	int row = array.size();
+	int col = array[0].size();
+	int i = 0;
+	while (i < row && col >= 0) {
+		if (array[i][col] > target) {
+			--col;
+		}
+		else if (array[i][col] < target) {
+			++i;
+		}
+		else {
+			return true;
+		}
+	}
+	return false;
+}
+
+int main() {
+	int array[3][3] = { {1,2,3},{4,5,6},{7,8,9} };
+	vector<vector<int>>vec(3,vector<int>(3));
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			vec[i][j] = array[i][j];
+		}
+	}
+	int target = 5;
+	bool flag = Find(target, vec);
+	if (flag) {
+		cout << "找到了" << endl;
+	}
+	else {
+		cout << "没找到" << endl;
+	}
+
+	system("pause");
+	return 0;
+}
+#endif
+#if 0
 int main() {
 	const char* p = "Heelo World1";
 
