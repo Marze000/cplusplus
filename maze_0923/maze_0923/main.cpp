e8d0104d169ca2207c7a103e6992a4b4194cd96a
Marze000 <1309334291@qq.com>
2019-09-27 21:17:00 +0800

    c++牛客网刷题

--- maze_0923/maze_0923/main.cpp
+++ maze_0923/maze_0923/main.cpp
@@ -6,10 +6,178 @@
 #include <algorithm>
 using namespace std;
 
+#if 1
+
+vector<int> printMatrix(vector<vector<int> > matrix) {
+	int row = matrix.size();
+	int col = matrix[0].size();
+	vector<int>temp;
+	if (row == 0 || col == 0) {
+		return temp;
+	}
+	int left = 0;
+	int right = col;
+	int up = 0;
+	int down = row;
+	while (left < right && up < down) {
+		//打印从左到右的
+		for (int i = left; i < right; ++i) {
+			temp.push_back(matrix[up][i]);
+		}
+		up++;
+		// 从上到下的
+		for (int i = up; i < down; ++i) {
+			temp.push_back(matrix[i][right - 1]);
+		}
+		--right;
+		//从右到左
+		for (int i = right - 1; i >= left && up != down; --i) {
+			temp.push_back(matrix[down - 1][i]);
+		}
+		--down;
+		for (int i = down - 1; i >= up && left != right; --i) {
+			temp.push_back(matrix[i][left]);
+		}
+		++left;
+	}
+	return temp;
+}
+
+int main() {
+	vector<vector<int>>vec(4,vector<int>(4,0));
+	int n = 1;
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			vec[i][j] = n;
+			++n;
+		}
+	}
+	printMatrix(vec);
+	system("pause");
+	return 0;
+}
+vector<int> printMatrix(vector<vector<int> > matrix) {
+	int row = matrix.size();
+	int col = matrix[0].size();
+	vector<int> res;
+
+	// 输入的二维数组非法，返回空的数组
+	if (row == 0 || col == 0)  return res;
+
+	// 定义四个关键变量，表示左上和右下的打印范围
+	int left = 0, top = 0, right = col - 1, bottom = row - 1;
+	while (left <= right && top <= bottom){
+		// left to right
+		for (int i = left; i <= right; ++i)
+			res.push_back(matrix[top][i]);
+		// top to bottom
+		for (int i = top + 1; i <= bottom; ++i)
+			res.push_back(matrix[i][right]);
+		// right to left
+		if (top != bottom)
+			for (int i = right - 1; i >= left; --i)
+				res.push_back(matrix[bottom][i]);
+		// bottom to top
+		if (left != right)
+			for (int i = bottom - 1; i > top; --i)  
+				res.push_back(matrix[i][left]);
+
+		left++, top++, right--, bottom--;
+	}
+	return res;
+}
+vector<int> printMatrix(vector<vector<int> > matrix) {
+	vector<int>res;
+	res.clear();
+	int row = matrix.size();//行数
+	int collor = matrix[0].size();//列数
+	//计算打印的圈数
+	int circle = ((row < collor ? row : collor) - 1) / 2 + 1;//圈数
+
+	for (int i = 0; i < circle; i++) {
+		//从左向右打印
+		for (int j = i; j < collor - i; j++)
+			res.push_back(matrix[i][j]);
+		//从上往下的每一列数据
+		for (int k = i + 1; k < row - i; k++)
+			res.push_back(matrix[k][collor - 1 - i]);
+		//判断是否会重复打印(从右向左的每行数据)
+		for (int m = collor - i - 2; (m >= i) && (row - i - 1 != i); m--)
+			res.push_back(matrix[row - i - 1][m]);
+		//判断是否会重复打印(从下往上的每一列数据)
+		for (int n = row - i - 2; (n > i) && (collor - i - 1 != i); n--)
+			res.push_back(matrix[n][i]);
+	}
+	return res;
+}
+class Solution {
+public:
+	bool HasSubtree(TreeNode* pRoot1, TreeNode* pRoot2){
+		if (pRoot2 == NULL || pRoot1 == NULL)
+			return false;
+
+		return dfs(pRoot1, pRoot2) 
+			|| HasSubtree(pRoot1->left, pRoot2)
+			|| HasSubtree(pRoot1->right, pRoot2);
+	}
+	bool dfs(TreeNode* node, TreeNode* pRoot2) {
+		if (pRoot2 == NULL)
+			return true;
+
+		else if (node != NULL) {
+			return (node->val == pRoot2->val) 
+				&& dfs(node->left, pRoot2->left)
+				&& dfs(node->right, pRoot2->right);
+		}
+		else 
+			return false;
+	}
+};
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+}; 
+
+class Solution {
+public:
+	bool isSubtree(TreeNode *t1, TreeNode *t2) {
+		// 意味着 t2 已近遍历完了
+		if (t2 == NULL) {
+			return true;
+		}
+		// 意味着 t1遍历完，但是t2还没有，所以返回false
+		if (t1 == NULL && t2 != NULL) {
+			return false;
+		}
+		if (t1->val == t2->val) {
+			bool lTree = isSubtree(t1->left, t2->left);
+			bool rTree = isSubtree(t1->right, t2->right);
+			return lTree && rTree;
+		}else {
+			return false;
+		}
+	}
+
+	bool HasSubtree(TreeNode* pRoot1, TreeNode* pRoot2){
+		//如果节点为空则返回nullptr
+		if (pRoot1 == NULL || pRoot2 == NULL) 
+			return false;
+
+		// 这个是由一定的顺序的，主要是找到值一样的节点
+		return isSubtree(pRoot1, pRoot2) 
+			|| HasSubtree(pRoot1->left, pRoot2)
+			|| HasSubtree(pRoot1->right, pRoot2);
+		// 直到找到两棵树值相等的两个节点
+	}
+};
+#endif
+
+#if 0
 struct ListNode {
 	int val;
 	ListNode *next;
-}; 
+};
 
 int main() {
 	ListNode* p1 = new ListNode;
@@ -35,7 +203,6 @@ int main() {
 	return 0;
 }
 
-#if 0
 
 void reOrderArray(vector<int> &array) {
 	vector<int>::iterator even = array.begin();
