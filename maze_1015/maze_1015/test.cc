179e38d4c898859363476522a930a5b3528b7804
Marze000 <1309334291@qq.com>
2019-10-28 23:14:06 +0800

    二叉树

--- maze_1015/maze_1015/test.cc
+++ maze_1015/maze_1015/test.cc
@@ -1,697 +1,86 @@
 #include <iostream>
-#include <algorithm>
 #include <stdlib.h>
-#include <stack>
 #include <vector>
-#include <string>
-#include <regex>
-#include <assert.h>
-#include <string>
 using namespace std;
 
-
-#if 0
-class Object;
-class SmartPointer;
-
-// 智能指针是一种资源管理类，通过对原始指针的封装，
-// 在资源管理类对象进行析构的时候对指针指向的内存
-// 进行释放，通常使用引用计数方式进行管理
-
-class Counter {
-	friend class SmartPointer;
-public:
-	Counter() {
-		ptr = nullptr;
-		cnt = 0;
-	}
-	Counter(Object*p) {
-		ptr = p;
-		cnt = 1;
-	}
-	~Counter() {
-		delete ptr;
-	}
-private:
-	Object*ptr;
-	int cnt;
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
 };
 
-class SmartPointer {
-public:
-	SmartPointer(Object* p) {
-		ptr_counter = new Counter(p);
+vector<int>vec;
+void Symmetrical(TreeNode* pRoot) {
+	if (pRoot->val == '#') {
+		return vec.push_back('#');
 	}
-	SmartPointer(const SmartPointer&sp) {
-		ptr_counter = sp.ptr_counter;
-		++ptr_counter->cnt;
-	}
-	SmartPointer& operator = (const SmartPointer& sp) {
-		++sp.ptr_counter->cnt;
-		--ptr_counter->cnt;
-		if (ptr_counter->cnt == 0) {
-			delete ptr_counter;
-		}
-		ptr_counter = sp.ptr_counter;
-	}
-	~SmartPointer() {
-		--ptr_counter->cnt;
-		if (ptr_counter->cnt == 0) {
-			delete ptr_counter;
-		}
-	}
-private:
-	Counter* ptr_counter;
-};
-
-
-bool duplicate(int numbers[], int length, int* duplication) {
-	// 2,3,1,0,2,5,3 
-	for (int i = 0; i < length; ++i) {
-		int index = numbers[i] % length;
-		if (numbers[index] >= length) {
-			*duplication = index;
-			return true;
-		}
-		numbers[index] += length;
-	}
-	return false;
+	Symmetrical(pRoot->left);
+	vec.push_back(pRoot->val);
+	Symmetrical(pRoot->right);
 }
 
-int find_dup(int numbers[], int length) {
-	// 2,3,1,0,2,5,3 
-	for (int i = 0; i < length; i++) {
-		int index = numbers[i];
-		if (index >= length) {
-			index -= length;
-			return index;
-		}
-		numbers[index] += length;
+bool isSymmetrical(TreeNode* pRoot) {
+	if (pRoot == nullptr) {
+		return true;
 	}
-	return -1;
-}
-#endif
-
-#if 0
-vector<int> multiply(const vector<int>& A) {
-	vector<int>B;
-	if (A.empty()) {
-		return B;
-	}
-	for (int i = 0; i < A.size(); ++i) {
-		int temp = 1;
-		for (int j = 0; j < A.size(); ++j) {
-			if (i == j && j < A.size()-1) {
-				++j;
-			}
-			temp *= A[j];
-		}
-		B.push_back(temp);
-	}
-	return B;
-}
-#endif
-
-void multiply(const vector<double>&array1, vector<double>& array2) {
-	int length1 = array1.size();
-	int length2 = array2.size();
-
-	if (length1 == length2 && length2 > 1) {
-		array2[0] = 1;
-		for (int i = 1; i < length1; ++i) {
-			array2[i] = array2[i - 1] * array1[i - 1];
-		}
-		double temp = 1;
-		for (int i = length1 - 2; i >= 0; --i) {
-			temp *= array1[i + 1];
-			array2[i] *= temp;
-		}
-	}
-}
-
-//bool isNumeric(const char* string) {
-//	if (!(*string == '+' || *string == '-' || (*string <= '9' && *string >= 0))) {
-//		return false;
-//	}
-//	++string;
-//	while (*string != '\0') {
-//		while (*string != '\0' && !(*string == 'e' || *string == 'E')) {
-//			if (!(*string >= '0' && *string <= '9')) {
-//				return false;
-//			}
-//			++string;
-//		}
-//		if (*string != '\0') {
-//			++string;
-//		}
-//		if (*string >= '0' && *string <= '9') {
-//			while (*string != '\0') {
-//				if (*string <= '9' && *string >= '0') {
-//					++string;
-//				}
-//				else {
-//					return false;
-//				}
-//			}
-//		}
-//	}
-//	return true;
-//}
-
-#if 0
-bool isNumeric(const char* string) {
-	// 如果第一个字符不是 +、- 、[0-9]直接返回false
-	if (!(isdigit(*string)
-		|| *string == '+' 
-		|| *string == '-')) {
-		return false;
-	}
-
-	// 第一个字符符合题意，接着往下走
-	++string;
-
-	// poilt 作为小数点的计数
-	// 如果大于 1 则返回false 
-	// 因为整个部分最多只可以出现一个小数点
-	int poilt = 0;
-
-	// 该循环的结束条件就是：
-	// (1)遇到 字符e、E   例：+123e   -12.34E
-	// (2)是一个正常的数字 例：123  +12.2   -12.3
-	while (*string != '\0' && *string != 'e' && *string != 'E') {
-		if (*string == '.') {
-			// 出现小数点便加1
-			poilt++;
-		}
-		// 如果小数点个数 > 1 或者当前字符不是数字或小说点则返回false
-		if ((poilt > 1) || !(isdigit(*string) || *string=='.')) {
+	Symmetrical(pRoot);
+	int start = 0;
+	int end = vec.size() - 1;
+	while (start < end) {
+		if (vec[start] != vec[end]) {
 			return false;
 		}
-		++string;
+		++start;
+		--end;
 	}
-
-	//这里我们需要判断上一个while 循环跳出的原因是什么？
-	if (*string != '\0') {
-		// 遇到了e
-		++string;
-	}
-	else {
-		// 是一个正常的数字
-		return true;
-	}
-	// 在 e 后面如果出现了 +、- 符号则直接跳过即可
-	if (*string == '-' || *string == '+') {
-		++string;
-	}
-
-	// 如果是数字则进行判断
-	if (isdigit(*string)) {
-		// 此时后面的字符必须全部都是数字,否则返回false
-		while (*string != '\0') {
-			if (!isdigit(*string)) {
-				return false;
-			}
-			++string;
-		}
-		return true;
-	}
-	else {
-		// 如果e 后面不是数字，不满足要求
-		return false;
-	}
+	return true;
 }
 
-class ListNode {
-public:
-	int val;
-	ListNode* next;
-};
 
+// 5, 5, 5, 5, #, #, 5, 5, #, 5
 int main() {
-	ListNode* pHead = nullptr;
-	ListNode* Head = new ListNode();
+	TreeNode* p1 = new TreeNode; p1->val = 5;
+	TreeNode* p2 = new TreeNode; p2->val = 5;
+	TreeNode* p3 = new TreeNode; p3->val = 5;
+	TreeNode* p4 = new TreeNode; p4->val = 5;
+	TreeNode* p5 = new TreeNode; p5->val = '#';
+	TreeNode* p6 = new TreeNode; p6->val = '#';
+	TreeNode* p7 = new TreeNode; p7->val = 5;
+	TreeNode* p8 = new TreeNode; p8->val = 5;
+	TreeNode* p9 = new TreeNode; p9->val = '#';
+	TreeNode* p10 = new TreeNode; p10->val = 5;
 
-	int* p = new int(10);
-	cout << *p;
-	system("pause");
-	return 0;
-}
+	TreeNode* p11 = new TreeNode; p11->val = '#';
+	TreeNode* p12 = new TreeNode; p12->val = '#';
+	TreeNode* p13 = new TreeNode; p13->val = '#';
+	TreeNode* p14 = new TreeNode; p14->val = '#';
+	TreeNode* p15 = new TreeNode; p15->val = '#';
+	p8->left = p11; p8->right = p12;
+	p10->left = p13; p10->right = p14;
+	p7->right = p15;
+	p1->left = p2; p1->right = p3;
+	p2->left = p4; p2->right = p5;
+	p3->left = p6; p3->right = p7;
+	p4->left = p8; p4->right = p9;
+	p7->left = p10;
 
-#endif
-//1、在经常需要搜索的列上，可以加快搜索的速度。 
-//2、在作为主键的列上，强制该列的唯一性和组织表中数据的排列结构。
-//3、在经常用于连接两张表的列上，这些列主要是一些外键，
-//   可以加快连接的速度。
-//4、在经常需要根据范围进行搜索的列上创建索引，
-//   因为索引已经排序，其指定的范围是连续的。
-//5、在经常需要排序的列上创建索引，因为索引已经排序，
-//   这样查询可以利用索引的排序，加快排序查询时间。
-//6、在经常使用在WHERE子句中的列上面创建索引，加快条件的判断速度。
-
-
-//int StrToInt(string str) {
-//	if (str.empty()) {
-//		return 0;
-//	}
-//	int symbol = 1;
-//	//处理负号
-//	if (str[0] == '-') {
-//		symbol = -1;
-//		str[0] = '0';
-//		//这里是‘0’ 不是0
-//	}
-//	//处理正号
-//	else if (str[0] == '+') {
-//		symbol = 1;
-//		str[0] = '0';
-//	}
-//	long long  sum = 0;
-//	for (int i = 0; i < str.size(); ++i) {
-//		if (str[i] < '0' || str[i] > '9') {
-//			sum = 0;
-//			break;
-//		}
-//		sum = sum * 10 + str[i] - '0';
-//		if (sum > 2147483647) {
-//			return 0;
-//		}
-//	}
-//	return symbol * sum;
-//}
-//
-//int StrToInt(string str) {
-//	if (str.empty()) {
-//		return 0;
-//	}
-//	int symbol = 1;
-//	//处理负号
-//	if (str[0] == '-') {
-//		symbol = -1;
-//		str[0] = '0'; 
-//		//这里是‘0’ 不是0
-//	}
-//	//处理正号
-//	else if (str[0] == '+') {
-//		symbol = 1;
-//		str[0] = '0';
-//	}
-//	int sum = 0;
-//	for (int i = 0; i < str.size(); ++i) {
-//		if (str[i] < '0' || str[i] > '9') {
-//			sum = 0;
-//			break;
-//		}
-//		sum = sum * 10 + str[i] - '0';
-//	}
-//	if (sum < INT_MIN || sum > INT_MAX) {
-//		return 0;
-//	}
-//	return symbol * sum;
-//}
-//
-//
-//int StrToInt(string str) {
-//	if (str.empty()) {
-//		return 0;
-//	}
-//
-//	// 如果字符串正有正负号则zhengfu为 true;
-//	// 如果是 '+' 则zhe为true
-//	// 如果是 '-' 则zhe为false
-//	bool zhengfu = false;
-//	bool zhe = true;
-//	if (str[0] == '-' ||str[0] =='+') {
-//		zhengfu = true;
-//		if(str[0] == '-'){
-//			zhe = false;
-//		}
-//	}
-//
-//	//字符转十进制的数
-//	bool nul = true;
-//	// nul 表示字符串是否有效，默认为有效
-//	int size = str.size();
-//	int n = size;
-//	long long num = 0;
-//
-//	//为了跳过第一个字符
-//	int j = 0;
-//	if (zhengfu) {
-//		j = 1;
-//	}
-//	for (int i = j; i < str.size(); ++i) {
-//		if (str[i] >= '0' && str[i] <= '9') {
-//			long long a = pow(10, size - 1);
-//			num += (str[i] - '0')*a;
-//			--size;
-//		}
-//		else {
-//			nul = false;
-//			break;
-//		}
-//	}
-//	// 字符串无效直接返回0 
-//	if (!nul) {
-//		return 0;
-//	}
-//	if (num / 10 == 2147483648) {
-//		return 0;
-//	}
-//	//如果有正号 则返回的值/10
-//	if (zhengfu && zhe) {
-//		return num/10;
-//	}
-//	if (!zhengfu && zhe) {
-//		return num;
-//	}
-//	if (zhengfu && !zhe) {
-//		return -(num / 10);
-//	}
-//	if (!zhengfu && zhe) {
-//		-num;
-//	}
-//}
-//
-
-//int StrToInt(string str) {
-//	if (str.empty()) {
-//		return 0;
-//	}
-//	int symbol = 1;
-//	//处理负号
-//	if (str[0] == '-') {
-//		symbol = -1;
-//		str[0] = '0';
-//		//这里是‘0’ 不是0
-//	}
-//	//处理正号
-//	else if (str[0] == '+') {
-//		symbol = 1;
-//		str[0] = '0';
-//	}
-//	long long sum = 0;
-//	for (int i = 0; i < str.size(); ++i) {
-//		if (str[i] < '0' || str[i] > '9') {
-//			sum = 0;
-//			break;
-//		}
-//		sum = sum * 10 + str[i] - '0';
-//		if (symbol && sum > 2147483647) {
-//			return 0;
-//		}
-//		if (!symbol && sum > 2147483649) {
-//			return 0;
-//		}
-//	}
-//	return symbol * sum;
-//}
-//
-#if 0
-int StrToInt(string str) {
-	if (str.empty()) {
-		return 0;
-	}
-	int symbol = 1;
-	//处理负号
-	if (str[0] == '-') {
-		symbol = -1;
-		str[0] = '0';
-		//这里是‘0’ 不是0
-	}
-	//处理正号
-	else if (str[0] == '+') {
-		symbol = 1;
-		str[0] = '0';
-	}
-	long long sum = 0;
-	for (int i = 0; i < str.size(); ++i) {
-		if (str[i] < '0' || str[i] > '9') {
-			sum = 0;
-			break;
-		}
-		sum = sum * 10 + str[i] - '0';
-	}
-	if ((symbol == 1) && sum > 2147483647) {
-		return 0;
-	}
-	if ((symbol ==-1) && sum >= 2147483649) {
-		return 0;
-	}
-
-	return symbol * sum;
-}
-
-int main() {
-	string str = "-2147483649";
-	//for (int i = 0; i < str.size(); ++i) {
-	//	str[i] = str[i] - 0;
-	//}
-	//cout << str << endl;
-
-	cout << StrToInt(str);
+	isSymmetrical(p1);
+	delete p1;
+	delete p2;
+	delete p3;
+	delete p4;
+	delete p5;
+	delete p6;
+	delete p7;
+	delete p8;
+	delete p9;
+	delete p10;
+	delete p11;
+	delete p12;
+	delete p14;
+	delete p13;
+	delete p15;
 
 	system("pause");
 	return 0;
 }
-
-class Solution {
-public:
-	Solution() {
-		++n;
-		sum += n;
-	}
-	void request() {
-		n = 0;
-		sum = 0;
-	}
-	int Sum_Solution(int n) {
-		request();
-		Solution a = new Solution[n];
-		delete[] a;
-		a = nullptr;
-		return sum;
-	}
-private:
-	int sum;
-	int n;
-};
-#endif
-
-// n = 5
-int Sum_Solution(int n) {
-	int ans = n;
-	ans && (ans+=Sum_Solution(n - 1));
-
-	return ans;
-}
-
-//int main() {
-//	cout << Sum_Solution(5) << endl;
-//
-//	system("pause");
-//	return 0;
-//}
-//
-//int LastRemaining_Solution(int n, int m) {
-//	if (n == 0 || m == 0) {
-//		return -1;
-//	}
-//	vector<int>vec;
-//	for (int i = 0; i < n; ++i) {
-//		vec.push_back(i);
-//	}
-//	int index = -1;
-//	auto iter = vec.begin();
-//	while (vec.size() > 1) {
-//		iter = vec.begin();
-//		index = (index + m) % vec.size();
-//		vec.erase(iter + index);
-//		--index;
-//	}
-//	return vec[0];
-//}
-//
-//
-//int main() {
-//	int n = 10;
-//	int m = 5;
-//	LastRemaining_Solution(n, m);
-//
-//	system("pause");
-//	return 0;
-//}
-//
-//bool IsContinuous(vector<int> numbers) {
-//	int size = numbers.size();
-//	sort(numbers.begin(), numbers.end());
-//	int zero_count = 0;
-//	for (int i = 0; i < size; ++i) {
-//		if (numbers[i] == 0) {
-//			++zero_count;
-//		}
-//	}
-//	int j = 0;
-//	for (int i = 0; i < size; ++i) {
-//		if (numbers[i] != 0) {
-//			j = i;
-//			break;
-//		}
-//	}
-//	int start = numbers[j];
-//	int end = numbers[size - 1];
-//	int nozero_num = end - start + 1;
-//	int shi_num = size - zero_count;
-//	if ((nozero_num - shi_num) <= zero_count) {
-//		return true;
-//	}
-//	else {
-//		return false;
-//	}
-//}
-////  0 0  5 6 8 10 11
-//int main() {
-//	vector<int>vec{0,0,0,1,1};
-//	if (IsContinuous(vec)) {
-//		cout << "有顺子" << endl;
-//	}
-//	else {
-//		cout << "没有顺子" << endl;
-//	}
-//	system("pause");
-//	return 0;
-//}
-//
-#if 0
-int main() {
-	string str = "i am a student!";
-	string s = str;
-	reverse(s.begin(), s.end());
-	cout << s << endl;
-
-	system("pause");
-	return 0;
-}
-
-class TreeNode {
-public:
-	TreeNode* left;
-	TreeNode* right;
-};
-
-bool isBalanced = true;
-bool IsBalanced_Solution(TreeNode* pRoot) {
-	getDepth(pRoot);
-	return isBalanced;
-}
-
-int getDepth(TreeNode *root) {
-	if (root == nullptr) {
-		return 0;
-	}
-	int left = getDepth(root->left);
-	int right = getDepth(root->right);
-	if (abs(left - right) > 1) {
-		isBalanced = false;
-	}
-	return right > left ? right + 1 : left + 1;
-}
-#endif
-
-//#if 0
-//void test1() {
-//	std::string tmp, html;
-//	while (getline(std::cin, tmp)) {
-//		tmp += '\n';
-//		html += tmp;
-//	}
-//	std::string pattern("http(s)?://([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?");
-//	pattern = "[[:alpha:]]*" + pattern + "[[:alpha:]]*";
-//	std::regex r(pattern);
-//	for (std::sregex_iterator it(html.begin(), html.end(), r), end;     //end是尾后迭代器，regex_iterator是regex_iterator的string类型的版本
-//		it != end; ++it)
-//	{
-//		std::cout << it->str() << std::endl;
-//	}
-//}
-//
-//void test2() {
-//	string regex_str("^(\\d{6})((1|2)\\d{3})((0|1)\\d)([0-3]\\d)(\\d{3}(X|\\d))$");
-//
-//	std::regex pattern(regex_str, std::regex::icase);
-//	//忽略字母大小写
-//
-//	string id("61251719901212444X");
-//	if (std::regex_match(id, pattern) == true) {
-//		cout << "匹配成功" << endl;
-//	}
-//
-//	// [a-zA-Z0-9_-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+  邮箱简单正则
-//	string mail_reg_str("^[a-zA-Z0-9_-]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+$");
-//	std::regex mail_reg(mail_reg_str, std::regex::icase);
-//
-//	assert(std::regex_match("chm--1989@163.com", mail_reg) == true);
-//}
-//
-//void IPTest(){
-//	//识别一个IP地址，并打印各个部分
-//	//输入exit退出程序
-//	bool isInputEnd = false;
-//	//(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})
-//	//有四个子表达式，括号中的内容为一个子表达式
-//	std::regex ip_reg("(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})");
-//	std::smatch matchResult;
-//	const string exitStr("EXIT");
-//	while (!isInputEnd){
-//		cout << "\nInput a IP address:";
-//		string inputStr;
-//		std::getline(std::cin, inputStr);
-//		if (inputStr.empty()){
-//			continue;
-//		}
-//
-//		string tmpStr(inputStr);
-//		std::transform(tmpStr.begin(), tmpStr.end(), tmpStr.begin(), toupper);
-//		if (tmpStr == exitStr){
-//			cout << "\nSYSTEM EXIT!";
-//			isInputEnd = true;
-//			continue;
-//		}
-//		//正则匹配
-//		if (std::regex_match(inputStr, matchResult, ip_reg)){
-//			cout << "Match: ";
-//			//打印子表达式结果
-//			for (size_t i = 1; i < matchResult.size(); ++i){
-//				cout << matchResult[i] << " ";
-//			}
-//		}
-//		else{
-//			cout << "Not Match!";
-//		}
-//	}
-//}
-//
-//
-//int main() {
-//	string str = "123143";
-//	string regex = "^[0-9]*&";
-//	regex_match(str,)
-//	//string regex_str = "runoo+b";
-//	//std::regex pattern(regex_str, std::regex::icase);
-//	//string id = "runoooob";
-//	//smatch res;
-//	//if (std::regex_match(id,res, pattern)) {
-//	//	cout << "匹配成功" << endl;
-//	//	for (size_t i = 0; i < res.size(); ++i) {
-//	//		cout << res[i] << ' ';
-//	//	}
-//	//	cout << endl;
-//	//}
-//	//else {
-//	//	cout << "没有匹配结果" << endl;
-//	//}
-//
-//	system("pause");
-//	return 0;
-//}
-//
-//#endif
