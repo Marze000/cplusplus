e68f3b208398a7293a7a587c7a4f5325b6b1eff6
Marze000 <1309334291@qq.com>
2019-11-14 23:46:06 +0800

    网易云注册界面

--- python/project/words.py
+++ python/project/words.py
@@ -1,25 +1,15 @@
-p = '''
-I heard the echo, from the valleys and the heart
-Open to the lonely soul of sickle harvesting
-Repeat outrightly, but also repeat the well-being of
-Eventually swaying in the desert oasis
-I believe I am Born as the bright summer flowers
-Do not withered undefeated fiery demon rule
-Heart rate and breathing to bear the load of the cumbersome
-Bored
-'''
+def permute(nums):
+    res = []
+    def defs(num,path):
+        if not num:
+            res.append(path)
+            return
+        for i in range(len(num)):
+            numss = num[:i]+num[i+1:]
+            defs(numss,path+[num[i]])
+    defs(nums,[])
+    return res
 
-# 统计一段文章中出现的单词个数
-lines = p.strip().split('\n')
-words_cnt = {}
-for line in lines:
-    line = line.replace(',', '').lower()
-    words = line.split(' ')
-    for word in words:
-        words_cnt[word] = words_cnt.get(word, 0) + 1
-
-words_lst = list(zip(words_cnt.values(), words_cnt.keys()))
-words_lst.sort()
-words_lst.reverse()
-for word in words_lst:
-    print(word[1], words_cnt[word[1]])
+if __name__ == "__main__":
+    num = [1,2,3,4,5]
+    print(permute(num))
