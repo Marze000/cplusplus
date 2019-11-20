0914a60e480b029e71cff0257e6844a19476c7c1
Marze000 <1309334291@qq.com>
2019-11-20 23:32:27 +0800

    链表练习

--- python/python_30/python_1.py
+++ python/python_30/python_1.py
@@ -169,9 +169,129 @@ if __name__ == "__main__":
     print(groupAnagrams(a))
 
 
-'''
-
 
 dit = {'123': 10, '2': 20, '3': 30}
 a = '123'
 print(dit.get(a, [123]))
+
+def maxSubArray(nums):
+    maxnum = nums[0]
+    res = nums[0]
+    for i in range(1,len(nums)):
+        if res>0:
+            res+=nums[i]
+        else:
+            res = nums[i] 
+        maxnum = max(res,maxnum)
+    return maxnum
+
+if __name__ == "__main__":
+    nums = [-2,1]
+    a = maxSubArray(nums)
+    print(a)
+
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+def merge(intervals):
+    if not intervals:
+        return intervals
+    intervals = sorted(intervals,  key=lambda x : x.start)
+    res = [intervals[0]]
+    for item in intervals[1:]:
+        if item.start <= res[-1].end:
+            res[-1].end = max(item.end, res[-1].end)
+        else:
+            res.append(item)
+    return res
+if __name__ == "__main__":
+    nums = [[1,3],[2,6],[8,10],[15,18]]
+    merge(nums)
+
+
+
+def lengthOfLastWord(s):
+    s = s.split()
+    return len(s[-1])
+
+
+if __name__ == "__main__":
+    s = '  hello world   ma     '
+    s = s.strip(' ')
+    print(s)
+    print(len(s))
+
+def generateMatrix(n):
+    res = [[0]*n for _ in range(n)]
+    i, j, di, dj = 0, 0, 0, 1
+    for k in range(1, n*n+1):
+        res[i][j] = k
+        if res[(i+di)%n][(j+dj)%n]:
+            di, dj = dj, -di
+        i += di
+        j += dj
+    return res
+
+if __name__ == "__main__":
+    a = generateMatrix(4)
+    print(a)
+
+import math
+def getPermutation(n, k):
+    kth = ''
+    nums = list(map(str, range(1, n+1)))
+    k -= 1
+    while n>0:
+        n -= 1
+        f = math.factorial(n)
+        index, k = divmod(k, f)
+        kth += nums.pop(index)        
+    return kth
+if __name__ == "__main__":
+    a = getPermutation(3,3)
+    print(a)
+    
+'''
+# Definition for singly-linked list.
+class ListNode:
+    val = x
+    next = None
+
+def rotateRight(head,k):
+    if not head or k==0:
+        return head
+    count = 0
+    temp_count = head
+    # 计算链表长度
+    while temp_count != None:
+        count+=1
+        temp_count=temp_count.next
+    # 如果k是链表长度的倍数则不翻转
+    if k%count==0:
+        return head
+
+    # 找到该从哪个位置断开链表
+    num = k%count
+    cut = head
+    n = count-num
+    while n > 0:
+        cut=cut.next
+        n-=1
+    ret = cut
+    cut = None
+    # 以下的任务是将 ret 的链表的尾部拼接到cut首部
+    retlist = ret
+    while ret.next !=None:
+        ret=ret.next
+    ret.next = head
+    return retlist
+
+if __name__ == "__main__":
+    n1 = new ListNode
+    n2 = new ListNode    
+    n3 = new ListNode    
+    n4 = new ListNode    
+    n5 = new ListNode    
