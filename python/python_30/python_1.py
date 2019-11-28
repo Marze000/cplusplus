f747b00226f6470560cc80049d4a664caeb0fc4b
Marze000 <1309334291@qq.com>
2019-11-28 23:51:46 +0800

    二叉树的遍历以及队列和列表应用

--- python/python_30/python_1.py
+++ python/python_30/python_1.py
@@ -1,3 +1,4 @@
+'''
 def levelOrder(self, root):
     if not root:
         return []
@@ -7,3 +8,14 @@ def levelOrder(self, root):
         LR_pair = [(node.left, node.right) for node in level]
         level = [leaf for LR in LR_pair for leaf in LR if leaf]   
     return res
+
+'''
+
+res = [(i*1, i*2)for i in range(10)]
+print(res)
+
+temp = []
+for i in res:
+    for j in i:
+        temp.append([j])
+print(temp)
