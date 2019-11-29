a8117342df4c1d89f4765fb045fa6b57ef6f0789
Marze000 <1309334291@qq.com>
2019-11-29 23:04:28 +0800

    二叉树

--- python/python_30/python_1.py
+++ python/python_30/python_1.py
@@ -1,21 +1,12 @@
-'''
-def levelOrder(self, root):
-    if not root:
+def postorderTraversal(self, root):
+    if root is None:
         return []
-    res, level = [], [root]
-    while level:
-        res.append([node.val for node in level])
-        LR_pair = [(node.left, node.right) for node in level]
-        level = [leaf for LR in LR_pair for leaf in LR if leaf]   
-    return res
-
-'''
-
-res = [(i*1, i*2)for i in range(10)]
-print(res)
-
-temp = []
-for i in res:
-    for j in i:
-        temp.append([j])
-print(temp)
+    stack, output = [root], []
+    while stack:
+        root = stack.pop()
+        output.append(root.val)
+        if root.left is not None:
+            stack.append(root.left)
+        if root.right is not None:
+            stack.append(root.right)
+    return output[::-1]
