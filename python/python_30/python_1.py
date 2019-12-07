1e8237d087fbf830ffa91b4fd77e74bb30cf9ef7
Marze000 <1309334291@qq.com>
2019-12-07 15:39:07 +0800

    求众数

--- python/python_30/python_1.py
+++ python/python_30/python_1.py
@@ -1,6 +1,18 @@
-res = []
-a = "a good example"
-s = a[::-1].split()
-for i in s:
-    res.append(i[::-1])
-print(res)
+def maxProduct(nums):
+    if not nums:
+        return 0
+    max_mult = nums[0]
+    result = nums[0]
+    for i in range(1, len(nums)):
+        result *= nums[i]
+        if result == 0:
+            result = 1
+            max_mult = max(0, max_mult)
+        max_mult = max(result, max_mult)
+    return max_mult
+
+
+if __name__ == "__main__":
+    nums = [-2, 0, -1]
+    a = maxProduct(nums)
+    print(a)
