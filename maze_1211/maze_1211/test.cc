4b1e380f3fbc826f63eeaee63aba4bba4e8d7d67
Marze000 <1309334291@qq.com>
2019-12-10 22:04:03 +0800

    虚函数和虚表

---
+++ maze_1211/maze_1211/test.cc
@@ -0,0 +1,141 @@
+﻿#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+#if 0
+class Caculator{
+public:
+	int a;int b;
+	int getResult(char fun) {
+		if (fun == '+') {
+			return a + b;
+		}
+		else if(fun == '-') {
+			return a - b;
+		}else if(fun == '*') {
+			return a * b;
+		}
+		/*
+		...
+		增加计算方式时  需要手动增删源代码 
+		...
+		*/
+	}
+};
+
+int main(){
+	char fun;
+	Caculator x;
+	x.a=5;
+	x.b=10;
+	cin >>fun;
+	cout << x.getResult(fun);
+	
+	system("pause");
+	return 0;
+}
+
+//父类计算器 
+class Caculator{
+public:
+	virtual int getResult(){
+		return 0;
+	}
+	int a;
+	int b;
+};
+//子类：加法器
+class Add :public Caculator{
+public:
+	int getResult(){
+		return a+b;
+	}
+};
+
+//此处省略减法器、乘法器等。
+int main() {
+	Caculator* c = new Caculator;
+	c->a = 1;
+	c->b = 2;
+	cout << c->getResult() << endl;
+
+	system("pause");
+	return 0;
+}
+
+class Car{
+public:
+	virtual void Drive() final {}
+};
+
+class Benz :public Car{
+public:
+	virtual void Drive() { cout << "Benz-舒适" << endl; }
+};
+
+class Car {
+public:
+	virtual void Drive()=0 {}
+};
+
+class Benz :public Car {
+public:
+	virtual void Drive() override {
+		cout << "Benz-舒适" << endl;
+	}
+};
+
+int main() {
+	Car *p = new Benz;
+	p->Drive();
+
+	system("pause");
+	return 0;
+}
+
+
+class Car{
+public:
+	virtual void Drive() = 0;
+};
+
+class Benz :public Car{
+public:
+	virtual void Drive(){
+		cout << "Benz-舒适" << endl;
+	}
+};
+
+class BMW :public Car{
+public:
+	virtual void Drive(){
+		cout << "BMW-操控" << endl;
+	}
+};
+
+void Test() {
+	Car* pBenz = new Benz;
+	pBenz->Drive();
+	Car* pBMW = new BMW;
+	pBMW->Drive();
+}
+
+
+
+
+#endif
+
+class Base{
+public:
+	virtual void Func1(){
+		cout << "Func1()" << endl;
+	}
+private:
+};
+
+int main() {
+	printf("%d\n",sizeof(Base));
+
+	system("pause");
+	return 0;
+}
--- python/python_30/python_1.py
+++ python/python_30/python_1.py
@@ -1,10 +1,3 @@
-def largestNumber(nums):
-    nums = [str(x)for x in nums]
-    nums = sorted(nums, cmp=lambda x, y: cmp(x+y, y+x), reverse=True)
-    return ''.join(nums).lstrip('0') or '0'
+nums = [1, 2, 3, 4, 5, 6]
 
-
-if __name__ == "__main__":
-    nums = [3, 30, 34, 5, 9]
-    a = largestNumber(nums)
-    print(a)
+print(nums[:])
