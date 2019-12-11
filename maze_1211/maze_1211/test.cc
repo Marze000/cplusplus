938d566e9f1f6200af4ce72b556700bce2876d53
Marze000 <1309334291@qq.com>
2019-12-11 22:21:48 +0800

    多态的规则

--- maze_1211/maze_1211/test.cc
+++ maze_1211/maze_1211/test.cc
@@ -123,7 +123,6 @@ void Test() {
 
 
 
-#endif
 
 class Base{
 public:
@@ -131,11 +130,41 @@ public:
 		cout << "Func1()" << endl;
 	}
 private:
+	int _b = 1;
 };
 
-int main() {
-	printf("%d\n",sizeof(Base));
+#endif
+
+class Base{
+public:
+	virtual void Func1(){
+		cout << "Base::Func1()" << endl;
+	}
+	virtual void Func2(){
+		cout << "Base::Func2()" << endl;
+	}
+	void Func3(){
+		cout << "Base::Func3()" << endl;
+	}
+private:
+	int _b = 1;
+};
+
+class Derive : public Base{
+public:
+	virtual void Func1(){
+		cout << "Derive::Func1()" << endl;
+	}
+private:
+	int _d = 2;
+};
+
+int main(){
+	Base b;
+	Derive d;
 
 	system("pause");
 	return 0;
 }
+		
+
