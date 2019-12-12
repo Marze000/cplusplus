a05b36b075ae262286884f793d62bf7e33e83197
Marze000 <1309334291@qq.com>
2019-12-12 21:28:20 +0800

    十进制转二进制

--- python/python_30/python_1.py
+++ python/python_30/python_1.py
@@ -2,9 +2,14 @@
 a = 10
 num = ''.join(bin(a))
 # print(list(num[::-1]))
-'''
 
 n = 10
 m = '{0:032b}'.format(n)[::-1]
 print(m)
 print(int(m, 2))
+
+
+'''
+nums = [1, 2, 3, 4, 5]
+num = nums[0:5:2]
+print(num)
