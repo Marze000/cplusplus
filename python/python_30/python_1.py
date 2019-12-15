5ab23897df9b68443b6686944fc7ad167b058530
Marze000 <1309334291@qq.com>
2019-12-15 19:31:06 +0800

    回文字符串的判断

--- python/python_30/python_1.py
+++ python/python_30/python_1.py
@@ -1,18 +1,12 @@
-def minSubArrayLen(s, nums):
-    if not sum or s>sum(nums):
-        return 0
-    left ,right,end = 0,1,len(nums)+1
-    min_num  = len(nums)
-    while right<end:
-        if sum(nums[left:right])>=s:
-            min_num = min(min_num,len(nums[left:right]))
-            left+=1
-        else:
-            right+=1
-    return min_num
-
+def shortestPalindrome(s):
+    r = s[::-1]
+    for i in range(len(s) + 1):
+        # startswith() 方法用于
+        # 检查字符串是否是以指定子字符串开头
+        # 如果是则返回 True，否则返回 False
+        if s.startswith(r[i:]):
+            return r[:i] + s
 if __name__ == "__main__":
-    s = 7
-    nums = [2,3,1,2,4,3]
-    a = minSubArrayLen(s, nums)
+    s = abcd
+    a = shortestPalindrome(s)
     print(a)
