19131fd479748cbb11f3a077bc9618a89a5aa888
Marze000 <1309334291@qq.com>
2019-12-15 20:55:11 +0800

    最大正方形

--- python/python_30/python_1.py
+++ python/python_30/python_1.py
@@ -1,12 +1,15 @@
-def shortestPalindrome(s):
-    r = s[::-1]
-    for i in range(len(s) + 1):
-        # startswith() 方法用于
-        # 检查字符串是否是以指定子字符串开头
-        # 如果是则返回 True，否则返回 False
-        if s.startswith(r[i:]):
-            return r[:i] + s
+def maximalSquare(matrix):
+    if not len(matrix) or not len(matrix[0]):
+        return 0
+    b,row,col = 0,len(matrix)+1,len(matrix[0])+1
+    res = [[0]*row for _ in range(col)]
+    for i in range(1,row):
+        for j in range(1,col):
+            if matrix[i-1][j-1]=='1':
+                res[i][j] = min(res[i-1][j-1],res[i-1][j],res[i][j-1])+1
+                b = max(b,res[i][j])
+    return b**2
 if __name__ == "__main__":
-    s = abcd
-    a = shortestPalindrome(s)
+    matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+    a = maximalSquare(matrix)
     print(a)
