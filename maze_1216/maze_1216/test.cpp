39dedbfffc1a5a23830d1711d0e3ce0059419adb
Marze000 <1309334291@qq.com>
2019-12-18 20:01:24 +0800

    map set

--- maze_1216/maze_1216/test.cpp
+++ maze_1216/maze_1216/test.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <stdlib.h>
+#include <string>
+#include <map>
+#include <set>
 using namespace std;
 
 #if 0
@@ -28,7 +31,6 @@ private:
 	}
 };
 
-#endif
 
 class A{
 protected:
@@ -47,11 +49,61 @@ public:
 	}
 };
 
-int main() {
-	A a;
-	a.create();
-	a.destory();
 
+int main() {
+	multimap<string, string>mp;
+	mp["apple"] = "苹果";
+	mp["peae"] = "梨";
+	mp["apricot"] = "杏";
+	mp["grape"] = "葡萄";
+	mp["pineapple"] = "菠萝";
+	mp["watermelon"] = "西瓜";
+	mp["orange"] = "橙子";
+	mp["lemon"] = "柠檬";
+	mp["mango"] = "芒果";
+	printf("%d\n", mp.size());
+
+	for (auto&e : mp) {
+		cout << e.first << "--->" << e.second << endl;
+	}
+	cout << endl;
+	mp.erase("pineapple");
+	mp["watermelon"] = "西瓜";
+	printf("%d\n", mp.size());
+	for (auto&e : mp) {
+		cout << e.first << "--->" << e.second << endl;
+	}
+	cout << endl;
+	cout << mp.at("apple")<< endl;
+	printf("%d\n", mp.size());
+
+	system("pause");
+	return 0;
+}
+
+#endif
+
+void TestSet(){
+	// 用数组array中的元素构造set
+	int array[] = {1,2,3,2,3,4,5};
+	multiset<int> s(array, array + sizeof(array) / sizeof(array[0]));
+	// 正向打印set中的元素，从打印结果中可以看出：set可去重
+	for (auto& e : s) {
+		cout << e << " ";
+	}
+	cout << endl;
+	// 直接把 value 插入进去即可
+	//s.insert(6);
+	//s.insert(7);
+	//// 使用迭代器逆向打印set中的元素
+	//for (auto it = s.rbegin(); it != s.rend(); ++it) {
+	//	cout << *it << " ";
+	//}
+	cout << endl;
+}
+
+int main() {
+	TestSet();
 	system("pause");
 	return 0;
 }
