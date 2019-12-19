0b4370efbe51601d0b2853bd78b5f7d29f057f03
Marze000 <1309334291@qq.com>
2019-12-19 23:13:27 +0800

    两个单词之间的最短距离

--- python/python_30/all_unique.py
+++ python/python_30/all_unique.py
@@ -1,16 +1,19 @@
+def shortestDistance(words, word1, word2):
+    res = len(words)
+    pos1, pos2 = None, None
+    for idx, word in enumerate(words):
+        if word == word1:
+            pos1 = idx
+        elif word == word2:
+            pos2 = idx
+        if pos1 is not None and pos2 is not None:
+            res = min(res, abs(pos1 - pos2))
+    return res
 
-'''
-# 判断列表中是否有重复的元素
-def all_unique(lit):
-    #如果没有重复的元素返回 True
-    # set 的作用是给列表去重
-    return len(lit) == len(set(lit))
 
-x = (1,2,3,4,5,2,5,6)
-
-for i in x:
-    if x.count(i) > 1:
-        print(i)
-        break
-'''
-
+if __name__ == "__main__":
+    words = ["practice", "makes", "perfect", "coding", "makes"]
+    word1 = "makes"
+    word2 = "coding"
+    a = shortestDistance(words, word1, word2)
+    print(a)
