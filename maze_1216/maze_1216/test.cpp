f4b689369cc4040d358f83fa34968ede6b48da43
Marze000 <1309334291@qq.com>
2019-12-20 23:16:50 +0800

    双端队列

--- maze_1216/maze_1216/test.cpp
+++ maze_1216/maze_1216/test.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <map>
 #include <set>
+#include <vector>
+#include <list>
 using namespace std;
 
 #if 0
@@ -81,7 +83,6 @@ int main() {
 	return 0;
 }
 
-#endif
 
 void TestSet(){
 	// 用数组array中的元素构造set
@@ -102,8 +103,117 @@ void TestSet(){
 	cout << endl;
 }
 
+
 int main() {
-	TestSet();
+	//int myints[] = {16,2,77,29 };
+	//int n = sizeof(myints) / sizeof(myints[0]);
+	//std::vector<int> fifth(myints, myints +n);
+	//fifth.push_back(10);
+	//fifth.push_back(20);
+	//fifth.push_back(30);
+	//std::vector<int>::iterator pos = find(fifth.begin(), fifth.end(), 77);
+	//std::vector<int>::iterator it = fifth.begin();
+	//for (; it != fifth.end(); ++it) {
+	//	cout << *it << ' ';
+	//}
+	//cout << endl;
+	//fifth.erase(pos);
+	//it = fifth.begin();
+	//for (; it != fifth.end(); ++it) {
+	//	cout << *it << ' ';
+	//}
+	//cout << endl;
+
+	int a[] = { 1, 2, 3, 4,5,6,7,8,9 };
+	vector<int> v(a, a + sizeof(a) / sizeof(int));
+	// 实现删除v中的所有偶数
+	// 下面的程序会崩溃掉，如果是偶数，erase导致it失效
+	// 对失效的迭代器进行++it，会导致程序崩溃
+	vector<int>::iterator it = v.begin();
+	while (it != v.end()){
+		if (*it % 2 == 0) {
+			it = v.erase(it);
+		}
+		++it;
+	}
+	for (it = v.begin(); it != v.end(); ++it) {
+		cout << *it << ' ';
+	}
+	cout << endl;
+	// 以上程序要改成下面这样，erase会返回删除位置的下一个位置
+	//vector<int>::iterator it = v.begin();
+	//while (it != v.end()){
+	//	if (*it % 2 == 0)
+	//		it = v.erase(it);
+	//	else
+	//		++it;
+	//}
+
+	system("pause");
+	return 0;
+}
+#endif
+class Date {
+public:
+	Date(int year,int month,int days)
+		:_year(year)
+		,_month(month)
+		,_days(days)
+	{
+		cout <<this<< "调用了构造函数" << endl;
+	}
+
+	Date(const Date& d)
+		:_year(d._year)
+		,_month(d._month)
+		,_days(d._days)
+	{
+		cout << this << "调用了拷贝构造函数" << endl;
+	}
+private:
+	int _year;
+	int _month;
+	int _days;
+};
+
+#include <queue>
+int main() {
+
+	deque<int>d1;
+	deque<int>d2(4, 100);
+	deque<int>d3(d2.begin(), d2.end());
+	deque<int>d4(d2);
+
+	//list<Date>l2;
+	//Date d(2019, 12, 20);
+	//l2.push_back(d);
+	//l2.emplace_back(2019,12,21);
+	//l2.emplace_front(2019, 12, 22);
+
+	//// 以数组为迭代器区间构造l5
+	//int array[] = { 16,2,77,29 };
+	//int n = sizeof(array) / sizeof(array[0]);
+	//std::list<int> l5(array, array + n);
+	//// list<int>::iterator it = l5.begin();
+	//auto it = l5.begin();
+	//while (it != l5.end()) {
+	//	l5.erase(it);
+	//	++it;
+	//}
+
+
+	///// 用迭代器方式打印l5中的元素
+	//std::list<int>::iterator it = l5.begin();
+	//for (; it != l5.end(); it++) {
+	//	std::cout << *it << " ";
+	//}
+	//std::cout << endl;
+	//for (auto& e : l5) {
+	//	std::cout << e << " ";
+	//}
+	//std::cout << endl;
+
+
 	system("pause");
 	return 0;
 }
