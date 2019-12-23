0c761669f6a164f4e6ac5265ddc79d34b9e61473
Marze000 <1309334291@qq.com>
2019-12-23 21:24:09 +0800

    重复的数字

--- python/python_30/all_unique.py
+++ python/python_30/all_unique.py
@@ -1,19 +1,16 @@
-def shortestDistance(words, word1, word2):
-    res = len(words)
-    pos1, pos2 = None, None
-    for idx, word in enumerate(words):
-        if word == word1:
-            pos1 = idx
-        elif word == word2:
-            pos2 = idx
-        if pos1 is not None and pos2 is not None:
-            res = min(res, abs(pos1 - pos2))
-    return res
+def moveZeroes(nums):
+    j = 0
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[j] = nums[i]
+            j += 1
+    while j < len(nums):
+        nums[j] = 0
+        j += 1
+    return nums
 
 
 if __name__ == "__main__":
-    words = ["practice", "makes", "perfect", "coding", "makes"]
-    word1 = "makes"
-    word2 = "coding"
-    a = shortestDistance(words, word1, word2)
+    nums = [0, 0, 1]
+    a = moveZeroes(nums)
     print(a)
--- python/python_30/python_1.py
+++ python/python_30/python_1.py
@@ -1,3 +1,20 @@
-nums = ['one', 'two', 'three', 'four']
-for index, num in enumerate(nums):
-    print(index, num)
+def uncommonFromSentences(A, B):
+    dic = {}
+    temp = A.split()+B.split()
+    for s in temp:
+        if s not in dic:
+            dic[s] = 1
+        else:
+            dic[s] += 1
+    res = []
+    for key, value in dic.items():
+        if value == 1:
+            res.append(key)
+    return res
+
+
+if __name__ == "__main__":
+    A = "this apple is sweet"
+    B = "this apple is sour"
+    a = uncommonFromSentences(A, B)
+    print(a)
