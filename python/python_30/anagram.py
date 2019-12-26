347bd20b696a06f37cd5a137497a092962b006d2
Marze000 <1309334291@qq.com>
2019-12-26 23:23:46 +0800

    零钱兑换

--- python/python_30/anagram.py
+++ python/python_30/anagram.py
@@ -1,19 +1,22 @@
-def lengthOfLIS(self, nums):
-    if not nums:
-        return 0
-    n = len(nums)
-    dp = [1]*n
-    for i in range(1, n):
-        cur_max = 0
-        for j in range(i):
-            if nums[i] > nums[j]:
-                cur_max = max(cur_max, dp[j])
-        if cur_max:
-            dp[i] = cur_max + 1
-    return max(dp)
+def coinChange(coins, amount):
+    dp = [0]*(amount+1)
+    coins.sort()
+    for i in range(1, amount+1):
+        if i in coins:
+            dp[i] = 1
+            continue
+        min_val = amount
+        for coin in coins:
+            if i >= coin:
+                min_val = min(min_val, dp[i-coin])
+            else:
+                break
+        dp[i] = min_val+1
+    return dp[amount] if dp[amount] <= amount else -1
 
 
 if __name__ == "__main__":
-    nums = [10, 9, 2, 5, 3, 7, 101, 18]
-    a = lengthOfLIS(nums)
+    coins = [1, 2, 5]
+    amount = 11
+    a = coinChange(coins, amount)
     print(a)
