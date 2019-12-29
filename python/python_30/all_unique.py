cc27bdc78d0cda5f5b46bcc3773130dd54f56c65
Marze000 <1309334291@qq.com>
2019-12-29 21:33:13 +0800

    在线笔试题17

--- python/python_30/all_unique.py
+++ python/python_30/all_unique.py
@@ -1,21 +1,3 @@
-def wordPattern(pattern, str):
-    s = str.split()
-    temp = {}
-    pattern = ' '.join(pattern)
-    pattern = list(pattern)
-    if len(set(pattern)) != len(set(s)):
-        return False
-    for i in range(len(pattern)):
-        if pattern[i] not in temp:
-            temp[pattern[i]] = s[i]
-        else:
-            if s[i] != temp[pattern[i]]:
-                return False
-    return True
 
-
-if __name__ == "__main__":
-    pattern = "abba"
-    str = "dog cat cat dog"
-    a = wordPattern(pattern, str)
-    print(a)
+num = ["h", "e", "l", "l", "o"]
+print(num[::-1])
