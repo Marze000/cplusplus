6401ba50db81a6fd74321e3193bd04c9bd11eed3
Marze000 <1309334291@qq.com>
2019-12-31 23:17:18 +0800

    求是否包含子串

--- python/python_30/all_unique.py
+++ python/python_30/all_unique.py
@@ -1,3 +1,34 @@
+'''
+def kthSmallest(matrix, k):
+    n, m = len(matrix), len(matrix[0])
+    l, h = matrix[0][0], matrix[n-1][m-1]
+    while l < h:
+        count = 0
+        mid = l + (h-l)//2
+        for i in range(n):
+            j = m-1
+            while j >= 0 and matrix[i][j] > mid:
+                j -= 1
+            count += j+1
+            print(count, k)
+        if count >= k:
+            h = mid
+        else:
+            l = mid + 1
+    return l
 
-num = ["h", "e", "l", "l", "o"]
-print(num[::-1])
+
+if __name__ == "__main__":
+    matrix = [
+        [1, 5, 9],
+        [10, 11, 13],
+        [12, 13, 15]
+    ]
+    k = 8
+    a = kthSmallest(matrix, k)
+    print(a)
+
+'''
+s = 'abcde'
+s = list(s)
+print(s)
