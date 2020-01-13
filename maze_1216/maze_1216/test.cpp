b8f837e2ee39604df4e0e54954f9b55ca6dfa7d2
Marze000 <1309334291@qq.com>
2020-01-13 22:09:24 +0800

    关联式容器unordered_map

--- maze_1216/maze_1216/test.cpp
+++ maze_1216/maze_1216/test.cpp
@@ -42,20 +42,78 @@ void Test_unordered_map_count() {
 		}
 	}
 }
-
 #endif
 
-int main() {
-	unordered_map<string, double> mymap = {
-		{"Burger",2.99},
-		{"Fries",1.99},
-		{"Soda",1.50} ,
-	};
-	mymap.insert({ "maze",2.55 });
-	for (auto&e:mymap) {
-		cout << e.first<<":"<<e.second << endl;
+#include <map>
+
+class Solution {
+public:
+	string frequencySort(string s) {
+		for (auto&e : s) {
+			mp[e]++;
+		}
+		for (auto p = mp.begin(); p != mp.end(); p++) {
+			multi.insert(make_pair(p->second, p->first));
+		}
+		int n = s.size() - 1;
+		for (auto p = multi.begin(); p != multi.end(); p++) {
+			int a = p->first;
+			char c = p->second;
+			while (a-- > 0) {
+				s[n--] = c;
+			}
+		}
+		return s;
 	}
+private:
+	map<char, int>mp;
+	multimap<int, char>multi;
+};
+
+#include <unordered_map>
+int main() {
+	string s = "trreddeeaattttaa";
+	map<char, int>mp;
+	multimap<int, char>multi;
+	for (auto&e : s) {
+		mp[e] += 1;
+	}
+
+	for (auto p = mp.begin(); p != mp.end(); ++p) {
+		multi.insert(make_pair(p->second,p->first));
+	}
+	
+	for (auto&e : mp) {
+		cout << e.first << " " << e.second << endl;
+	}
+
 	cout << endl;
+
+	for (auto&e : multi) {
+		cout << e.first << " " << e.second << endl;
+	}
+
+
+	/*for (auto&e : mp) {
+		cout << e.first <<" " <<e.second << endl;
+
+		a 4
+		d 2
+		e 3
+		r 2
+		t 5
+	*/
+	cout << endl;
+	//unordered_map<string, double> mymap = {
+	//	{"Burger",2.99},
+	//	{"Fries",1.99},
+	//	{"Soda",1.50} ,
+	//};
+	//mymap.insert({ "maze",2.55 });
+	//for (auto&e:mymap) {
+	//	cout << e.first<<":"<<e.second << endl;
+	//}
+	//cout << endl;
 
 
 
