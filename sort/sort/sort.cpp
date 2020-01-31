d856ac6f6a477af03ca0589af24eba5dd035d805
Marze000 <1309334291@qq.com>
2020-01-31 17:17:52 +0800

    归并排序，计数排序

--- sort/sort/sort.cpp
+++ sort/sort/sort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <algorithm>
+#include <assert.h>
 using namespace std;
 
 #if 0
@@ -56,7 +57,6 @@ void HeapSort(int* array, int size) {
 		end--;
 	}
 }
-#endif
 
 
 int Partion2(int *array, int left, int right) {
@@ -67,31 +67,130 @@ int Partion2(int *array, int left, int right) {
 		while (begin < end && array[begin] <= key) {
 			begin++;
 		}
-		array[end] = array[begin];
-		end--;
+		if (begin < end) {
+			array[end] = array[begin];
+			end--;
+		}
 		while (begin < end && array[end] >= key) {
 			--end;
 		}
-		array[begin] = array[end];
-		begin++;
+		if (begin < end) {
+			array[begin] = array[end];
+			begin++;
+		}
 	}
 	array[begin] = key;
 	return begin;
+}
+
+int	Partion3(int *array, int left, int right) {
+	int key = array[right - 1];
+	int pre = left - 1;
+	for (int cur = left; cur < right; ++cur) {
+		if (array[cur] < key && ++pre != cur) {
+			swap(array[cur], array[pre]);
+		}
+	}
+	swap(array[++pre], array[right - 1]);
+	return pre;
 }
 
 void QuickSort(int* array, int left, int right) {
 	if (right - left <= 1) {
 		return;
 	}
-	int div = Partion2(array, left, right);
+	int div = Partion3(array, left, right);
 	// 然后分别对 [left,div] 和 [div+1,right] 中的元素排序
 	QuickSort(array, left, div);
 	QuickSort(array, div + 1, right);
 }
 
+void MergeData(int *array, int left, int mid, int right, int *temp) {
+	int begin1 = left, end1 = mid;
+	int begin2 = mid, end2 = right;
+	int index = left;
+	while (begin1 < end1 && begin2 < end2) {
+		if (array[begin1] < array[begin2]) {
+			temp[index++] = array[begin1++];
+		}
+		else {
+			temp[index++] = array[begin2++];
+		}
+	}
+	while (begin1 < end1) {
+		temp[index++] = array[begin1++];
+	}
+	while (begin2 < end2) {
+		temp[index++] = array[begin2++];
+	}
+}
+
+void _MergeSort(int* array, int left, int right, int* temp) {
+	if (right - left <= 1) {
+		return;
+	}
+	int mid = (left + right) / 2;
+	_MergeSort(array, left, mid, temp);
+	_MergeSort(array, mid, right, temp);
+	MergeData(array, left, mid, right, temp);
+	memcpy(array + left, temp + left, sizeof(array[left]) * (right - left));
+}
+
+void MergeSort(int* array, int size) {
+	int* temp = (int*)malloc(size * sizeof(array[0]));
+	if (temp == NULL) {
+		assert(0);
+		return;
+	}
+	_MergeSort(array, 0, size, temp);
+	free(temp);
+}
+#endif
+
+void CountSort(int *array, int size) {
+	//选择最小的和最大的数字·O（N）	N代表的是数据个数
+	int MaxValue = array[0];
+	int MinValue = array[0];
+
+	for (int i = 1; i < size; ++i) {
+		if (array[i] > MaxValue) {
+			MaxValue = array[i];
+		}
+
+		if (array[i] < MinValue) {
+			MinValue = array[i];
+		}
+	}
+
+	//申请空间·O（M）[辅助空间]·M代表是数据范围
+	int Range = MaxValue - MinValue + 1;
+	int* array2 = (int*)malloc(sizeof(int)*Range);
+	if (array2 == NULL) {
+		assert(0);
+		return;
+	}
+
+	//将每一个位置置空
+	memset(array2, 0, sizeof(int)*Range);
+
+	//统计元素出现次数·O(N)
+	for (int i = 0; i < size; ++i) {
+		array2[array[i] - MinValue]++;
+	}
+
+	//往出搬移元素·O（N）
+	int index = 0;
+	for (int i = 0; i < Range; ++i) {
+		while (array2[i]--) {
+			array[index++] = i + MinValue;
+		}
+	}
+	free(array2);
+}
+
 int main() {
 	int arr[] = { 1,3,4,6,2,7,9,0,8,5 };
-	QuickSort(arr, 0, 10);
+	CountSort(arr,10);
 	for (int i = 0; i < 10; ++i) {
 		cout << arr[i] << ' ';
 	}
