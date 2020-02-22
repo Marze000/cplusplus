f3cd4f22018381130ced93e6b6b67432ce7c6c54
Marze000 <1309334291@qq.com>
2020-02-22 17:59:53 +0800

    将排序二叉树转换为排序的双向链表

--- 2010_0215/2010_0215/test.cpp
+++ 2010_0215/2010_0215/test.cpp
@@ -5,21 +5,49 @@
 #include <algorithm>
 using namespace std;
 
-struct Cmp {
-	bool operator()(const int a, const int b) {
-		return a > b;
+void MergeData(int* arr, int left, int mid, int right, int* temp) {
+	int begin1 = left;
+	int begin2 = mid;
+	int end1 = mid;
+	int end2 = right;
+	int index = left;
+	while (begin1 < end1 && begin2 < end2) {
+		if (arr[begin1] < arr[begin2]) {
+			temp[index++] = arr[begin1++];
+		}
+		else{
+			temp[index++] = arr[begin2++];
+		}
 	}
-}cmp;
+	while (begin1 < end1) {
+		temp[index++] = arr[begin1++];
+	}
+	while (begin2 < end2) {
+		temp[index++] = arr[begin2++];
+	}
+}
 
+void MergeSort(int *arr, int left, int right, int* temp) {
+	if (right - left <= 1) {
+		return;
+	}
+	int mid = left + (right - left) / 2;
+	MergeSort(arr, left, mid, temp);
+	MergeSort(arr, mid,right, temp);
+	MergeData(arr, left, mid, right, temp);
+	memcpy(arr + left, temp + left, sizeof(arr[0])*(right - left));
+}
 int main() {
-	vector<int>v{ 1,3,4,5,2,6 };
-	replace(v.begin(), v.end(), 6, 600);
-	for (auto&e : v) {
+	int arr[10] = { 0,2,4,6,8,1,3,5,7,9 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	int* temp = new int[sizeof(arr)];
+	MergeSort(arr, 0, size, temp);
+	for (auto&e : arr) {
 		cout << e << ' ';
 	}
 	cout << endl;
+	delete[] temp;
 
 	system("pause");
 	return 0;
 }
-
