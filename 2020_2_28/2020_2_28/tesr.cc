41a4790282e9f64606fa1f7079a882fc59f23373
Marze000 <1309334291@qq.com>
2020-04-09 22:11:21 +0800

    字符串的分割

--- 2020_2_28/2020_2_28/tesr.cc
+++ 2020_2_28/2020_2_28/tesr.cc
@@ -1,29 +1,46 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 #include <algorithm>
-#include <stack>
 using namespace std;
 
-int largestRectangleArea(vector<int>& heights) {
-	heights.push_back(0);
-	stack<int> stk;
-	int maxArea = 0;
-	for (int i = 0; i < heights.size(); i++) {
-		while (!stk.empty() && heights[i] < heights[stk.top()]) {
-			int top = stk.top();
-			stk.pop();
-			maxArea = max(maxArea, heights[top] * (stk.empty() ? i : (i - stk.top() - 1)));
+bool ispalindrome(string s, int i, int j) {
+	while (i < j) {
+		if (s[i] != s[j]) {
+			return false;
 		}
-		stk.push(i);
+		++i; --j;
 	}
-	return maxArea;
+	return true;
 }
-int main() {
-	vector<int>heights = { 2,1,5,6,2,3 };
-	cout << largestRectangleArea(heights) << endl;
+int minCut(string s) {
+	if (s.empty()) return 0;
+	int n = s.size();
+	vector<int>dp(n + 1, 0);
+	dp[0] = -1;// 如果没有这一项，aaaaaaa会产生错误
+	// dp[i] 表示到第 i 个字符需要的最小分割次数
+	for (int i = 1; i <= n; ++i) {
+		dp[i] = i - 1;
+	}
 
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 0; j < i; ++j) {
+			// dp(i) = min(dp(i),1+dp(j)) 
+			if (ispalindrome(s, j, i - 1)) {
+				dp[i] = min(dp[i], 1 + dp[j]);
+			}
+		}
+	}
+	return dp[n];
+}
+
+
+int main() {
+	string s = "aaaaaa";
+
+	cout << minCut(s) << endl;
 
 	system("pause");
 	return 0;
 }
+
